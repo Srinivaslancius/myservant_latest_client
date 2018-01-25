13faa35375ddb0e43bd9ed04159c9fba0129df5c
srinivaslancius <srinivas@lanciussolutions.com>
2018-01-25 15:53:03 +0530

    changes in groceries

--- order_details.php
+++ order_details.php
@@ -101,45 +101,74 @@
                       <div class="panel-body">
                      <div class="table-responsive">	
 					<?php $order_id = $_GET['order_id'];
-		          $getOrders = "SELECT * FROM food_orders WHERE order_id='$order_id'";
-		          $groceryOrdersData = $conn->query($getOrders);
-		          $i=1;
-		        ?>	 
+					//echo "<script>alert('$order_id')</script>";
+		          
+    $groceryOrders1 = "SELECT * FROM grocery_orders WHERE  order_id = '$order_id' "; 
+    $groceryOrdersData1 = $conn->query($groceryOrders1);
+    $i=1; 
+    $OrderDetails = $groceryOrdersData1->fetch_assoc();
+    $getSiteSettingsData = getIndividualDetails('grocery_site_settings','id',1);
+
+    $getProducts = getIndividualDetails('grocery_product_name_bind_languages','product_id',$OrderDetails['product_id']);
+
+    $getpaymentTypes = getIndividualDetails('lkp_payment_types','id',$OrderDetails['payment_method']);
+
+    $orderStatus = getIndividualDetails('lkp_order_status','id',$OrderDetails['lkp_order_status_id']);
+
+    $service_tax = $OrderDetails['sub_total']*$getSiteSettingsData['service_tax']/100;
+
+if($OrderDetails['delivery_charges'] == '0') {
+  $order_type = "Take Away";
+  $delivery_charges = 0;
+} else {
+  $order_type = "Delivery";
+  $delivery_charges = $OrderDetails['delivery_charges'];
+}
+                          ?>
+
         			<table class="table" style="border:1px solid #ddd;width:100%">
             		<tbody>
 		  <tr>
 			<td colspan="2" style="padding-left:20px">
 			<h3>Order Information</h3><br>
-			<p>Restaurant Name: Sweet Magic</p>
-			<p>Payment Method:Cash On Delivery</p>
-			<p>Order Type:Take Away</p>
-			<p>Order Status: InProgress</p>
+			<p>Item Name: <?php echo $getProducts['product_name'] ?></p>
+			<p>Payment Method:<?php echo $getpaymentTypes['status']; ?></p>
+			<p>Order Type:<?php echo $order_type; ?></p>
+			<p>Order Status: <?php echo $orderStatus['order_status']; ?></p>
 			<p>Payment Status: InProgress</p></td>
 			<td colspan="2"></td>
 			<td colspan="2">
 			<h3>Shipping Address</h3><br>
-			<p>srinu</p>
-			<p>srinivas@lanciussolutions.com</p>
-			<p>7894561235</p>
-			<p>test</p>
-			<p>345345</td>
+			<p><?php echo $OrderDetails['first_name']; ?></p>
+		      <p><?php echo $OrderDetails['email']; ?></p>
+		      <p><?php echo $OrderDetails['mobile']; ?></p>
+		      <p><?php echo $OrderDetails['address']; ?></p>
+		      <p><?php echo $OrderDetails['postal_code']; ?></p></td>
 		  </tr>
 		  <tr>
-			<td><h5>ITEM NAME</h5></td>
-			<td><h5>CUSINE TYPE</h5></td>
-			<td><h5>ITEM WEIGHT</h5></td>
+			<td colspan="2"><h5>ITEM NAME</h5></td>
+			<td colspan="2"><h5>CATEGORY</h5></td>
 			<td><h5>QUANTITY</h5></td>
 			<td><h5>PRICE</h5></td>
-			<td><h5>TOTAL</h5></td>
+			<td></td>
+			<td></td>
 		  </tr>
+		  <?php $getOrders1 = "SELECT * FROM grocery_orders WHERE order_id='$order_id'";
+    			$getOrdersData3 = $conn->query($getOrders1); ?>
+    	<?php while($getOrdersData2 = $getOrdersData3->fetch_assoc()) { 
+        $getCategories = getIndividualDetails('grocery_category','id',$getOrdersData2['category_id']);
+        $getProducts1 = getIndividualDetails('grocery_products','id',$getOrdersData2['product_id']);
+        $getItemWeights = getIndividualDetails('food_product_weights','id',$getOrdersData2['item_weight_type_id']);
+      	?>
 		   <tr>
-			<td><p>Biryani</p></td>
-			<td><p>MAIN COURSES1</p></td>
-			<td><p>Medium</p></td>
-			<td><p>3</p></td>
-			<td><p>80</p></td>
-			<td><p>240</p></td>
+			<td colspan="2"><p><?php echo $getProducts['product_name'] ?></p></td>
+			<td colspan="2"><p><?php echo  $getCategories['category_name']?></p></td>
+			<td><p><?php echo $getOrdersData2['item_quantity'] ?></p></td>
+			<td><p><?php echo $getOrdersData2['item_price'] ?></p></td>
+			<td></td>
+			<td></td>
 		  </tr>
+		  <?PHP } ?>
 		   <tr>
 			<td></td>
 			<td></td>
@@ -147,11 +176,14 @@
 			<td></td>
 			<td><p>Subtotal:</p>
 			<p>Tax:</p>
+			<p>Delivery Charges</p>
 			<p style="color:#fe6003;">Grand Total:</p></td>
-			<td><p style="color:#fe6003;">Rs. 240</p>
-			<p style="color:#fe6003;">Rs. 24(10 %)</p>
-			
-				<p style="color:#fe6003;">Rs. 264</p>
+			<td><p style="color:#fe6003;">Rs. <?php echo $OrderDetails['sub_total'];  ?></p>
+
+			<p style="color:#fe6003;">Rs. <?php echo $OrderDetails['service_tax'].'('.$getSiteSettingsData['service_tax'].'%)' ?></p>
+			<p style="color:#fe6003;">Rs. <?php echo $OrderDetails['delivery_charges'];  ?></p>
+			<?php $total_price = round($OrderDetails['sub_total'] + $OrderDetails['delivery_charges'] + $OrderDetails['service_tax']); ?>
+				<p style="color:#fe6003;"><?php echo $total_price;  ?></p>
 		  </tr>
 		</tbody>
 					
