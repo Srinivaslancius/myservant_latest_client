efa03f9481f5891ef0b73ac9c15528d9d12ea887
srinivaslancius <srinivas@lanciussolutions.com>
2018-01-25 16:44:49 +0530

    changes in wallet

--- wallet.php
+++ wallet.php
@@ -93,50 +93,38 @@
             			<th></th>
 						<th></th>
             		  </tr>
-            		</thead>
-
-            		<?php 
-
-					if(isset($_POST["submit"]) && $_POST["submit"]!="") {
-
-						//echo "<pre>"; print_r($_POST); die;
-
-						$user_id = $_SESSION['user_login_session_id'];
-						$wallet_id = $_SESSION['wallet_id'];
-						$credit_amnt = $_POST['amnt'];						
-						$description = "Money Added in Wallet";
-						$updated_date = date('Y-m-d H:i:s', time() + 24 * 60 * 60);
-
-            			$sqlInwallet = "INSERT INTO `user_wallet_transactions`( `wallet_id`, `user_id`, `credit_amnt`, `description`, `lkp_payment_status_id`, `updated_date`) VALUES ('$wallet_id','$user_id','$credit_amnt','$description','2','$updated_date')";
-            			if($conn->query($sqlInwallet) === TRUE) {
-            				$last_id = $conn->insert_id;
-            				header("Location: PayUMoney_form_wallet.php?key=".$last_id."");
-            			}
-            		}
-            		$user_id = $_SESSION['user_login_session_id'];
-					$wallet_id = $_SESSION['wallet_id'];
-            		$getwalletAmount = "SELECT * FROM user_wallet WHERE user_id = '$user_id' AND wallet_id = '$wallet_id'";
-            		$getwalletAmount1 = $conn->query($getwalletAmount);
-            		$getwalletAmountDetails = $getwalletAmount1->fetch_assoc();
-            		if($getwalletAmountDetails['amount'] == '') {
-            			$amount = 0;
-            		} else {
-            			$amount = $getwalletAmountDetails['amount'];
-            		}
-            		?>
-            		<form method="post" >
+            		</thead>           		
             		<tbody>
             		  <tr>
-            			<td style="width:10%"><img src="images/dashboard/wallet.png" style="width:50px;height:50px"></td>
-            			<td><b>Rs : <?php echo $amount; ?>/-</b><br>Your Wallet Balance</td>
-            			<td colspan="2"><input type="text" name="amnt" id="amnt" class="valid_mobile_num" placeholder="Enter amount to be added in your wallet" required></td>						
+            			<td><img src="images/dashboard/wallet.png"></td>
+            			<td><b>Rs :5/-</b><br>Your Wallet Balance</td>
+            			<td colspan="2"><input type="text" name="amnt" placeholder="Enter amount to be added in your wallet" required></td>						
 						<td><button class="button1" type="submit" name="submit" value="submit">Add Money to Wallet</button></td>
             		  </tr>            		  
             		</tbody>
-            		</form>
+            	
 					
         	     </table>
-				 <table class="table" style="border:1px solid #ddd;width:100%">
+				  </div>
+				  </div>
+				   <div class="panel-body">
+		<section class="flat-imagebox style2 background">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="product-wrap">
+							<div class="product-tab style1">
+								<ul class="tab-list">
+									<li class="active">Somthing</li>
+									<li>Somthing</li>
+									
+								</ul><!-- /.tab-list -->
+							</div><!-- /.product-tab style1 -->
+							<div class="tab-item">
+								<div class="row">
+									<div class="col-md-12">
+										 <div class="table-responsive">	
+											<table class="table" style="border:1px solid #ddd;width:95%;margin-left:20px">
             		<thead>
             		  <tr>
             			<th>MERCHANT NAME</th>
@@ -154,25 +142,72 @@
 						<td>SUCCESS</td>
 						<td>Order #4419408824 of Reacharge of Airtel Mobile 730214...(Promocode:GETS)</td>
             		  </tr>
-					  <tr>
+					  <!--<tr>
+            			<td><b>Cashback Received</b><br>paytm for Order #CASH-676607643 Paytm Cash Txn ID 17376641204 2018-01-09 09:39:13 PM</td>
+            			<td></td>
+            			<td>Rs : 5/-</td>
+						<td>SUCCESS</td>
+						<td>Order #4419408824 of Reacharge of Airtel Mobile 730214...(Promocode:GETS)</td>
+            		  </tr>-->
+            		  
+            		</tbody>
+					
+        	     </table>
+				 
+										
+									</div><!-- /.col-md-6 -->
+									
+								</div><!-- /.row -->
+								</div>
+											<div class="row">
+									<div class="col-md-12">
+										 <div class="table-responsive">	
+											<table class="table" style="border:1px solid #ddd;width:95%;margin-left:20px">
+            		<thead>
+            		  <tr>
+            			<th>MERCHANT NAME</th>
+            			<th>WITHDRAWAL</th>
+            			<th>DEPOSIT</th>
+            			<th>STATUS</th>
+						<th>COMMENT</th>
+            		  </tr>
+            		</thead>
+            		<tbody>
+            		  <tr style="border-bottom:1px solid #ddd">
             			<td><b>Cashback Received</b><br>paytm for Order #CASH-676607643 Paytm Cash Txn ID 17376641204 2018-01-09 09:39:13 PM</td>
             			<td></td>
             			<td>Rs : 5/-</td>
 						<td>SUCCESS</td>
 						<td>Order #4419408824 of Reacharge of Airtel Mobile 730214...(Promocode:GETS)</td>
             		  </tr>
+					  <!--<tr>
+            			<td><b>Cashback Received</b><br>paytm for Order #CASH-676607643 Paytm Cash Txn ID 17376641204 2018-01-09 09:39:13 PM</td>
+            			<td></td>
+            			<td>Rs : 5/-</td>
+						<td>SUCCESS</td>
+						<td>Order #4419408824 of Reacharge of Airtel Mobile 730214...(Promocode:GETS)</td>
+            		  </tr>-->
             		  
             		</tbody>
 					
         	     </table>
+				 
+										
+									</div><!-- /.col-md-6 -->
+									
+								</div><!-- /.row -->
+							</div><!-- /.tab-item -->
+						</div><!-- /.product-wrap -->
+					</div><!-- /.col-md-12 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+			</div>
+		</section><!-- /.flat-imagebox style2 -->
         	  </div>
                       </div>
                   </div>
-                  
-                </div><!-- End panel-group -->
                 
-            
-        </div><!-- End col-md-9 -->
+                </div>
     </div><!-- End row -->
 			</div><!-- /.container -->
 		</section><!-- /.flat-term-conditions -->
