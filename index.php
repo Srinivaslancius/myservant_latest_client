adbf62d4a44d10e7e92f572387dae6fed5425480
srinivaslancius <srinivas@lanciussolutions.com>
2018-01-27 15:17:09 +0530

    changed index page

--- index.php
+++ index.php
@@ -227,19 +227,19 @@ $tagNames = $conn->query($getTags);
 											</div>
 										</div><!-- /.box-content -->
 										<div class="box-bottom">
-										<div class="row">
-										<div class="col-sm-5 col-xs-12">
-										<div class="quanlity">
-										<input name="product_quantity" value="1" min="1" max="20" placeholder="Quantity" id="product_quantity"type="number" style="height:45px">
-											</div>							
-										</div>
-										<div class="col-sm-7 col-xs-12" style="margin-left:-20px">
-											<div class="btn-add-cart mrgn_lft">
-												<a href="#" title="" onClick="show_cart(<?php echo $productDetails['id']; ?>)" style="width:115%">
-													<img src="images/icons/add-cart.png" alt="">Add to Cart
-												</a>
-											</div>
-											</div>
+											<div class="row">
+												<div class="col-sm-5 col-xs-12">
+													<div class="quanlity">
+														<input name="product_quantity" value="1" min="1" max="20" placeholder="Quantity" id="product_quantity_<?php echo $productDetails['id']; ?>" type="number" style="height:45px">
+													</div>							
+												</div>
+												<div class="col-sm-7 col-xs-12" style="margin-left:-20px">
+													<div class="btn-add-cart mrgn_lft">
+														<a href="#" title="" onClick="show_cart(<?php echo $productDetails['id']; ?>)" style="width:115%">
+															<img src="images/icons/add-cart.png" alt="">Add to Cart
+														</a>
+													</div>
+												</div>
 											</div>
 											<div class="compare-wishlist">
 												<a href="#" class="compare" title="">
@@ -703,8 +703,7 @@ if($getTodayDeals1->num_rows > 0) { ?>
 				var split = product.split(",");
 				var productWeightType = split[0];
 				var productPrice = split[1];
-				var product_quantity = 1;
-
+				var product_quantity = $('#product_quantity_'+ProductId).val();
 	   			$.ajax({
 			      type:'post',
 			      url:'save_cart.php',
