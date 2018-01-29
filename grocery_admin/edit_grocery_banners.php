0467c5a78147460b3aef2e2072ba66e976b696a9
srinivaslancius <srinivas@lanciussolutions.com>
2018-01-29 15:39:29 +0530

    changed name in banners page

--- grocery_admin/edit_grocery_banners.php
+++ grocery_admin/edit_grocery_banners.php
@@ -86,7 +86,7 @@
         <div class="site-content">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="m-y-0 font_sz_view">Edit Brands</h3>
+                    <h3 class="m-y-0 font_sz_view">Edit Banners</h3>
                 </div>
                 <div class="panel-body">
                     <div class="row">
