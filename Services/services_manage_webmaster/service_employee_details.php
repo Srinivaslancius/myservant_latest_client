ee637935606f9e47851a08f0493d70e426a2d649
Srinivaslancius <srinivas@lanciussolutions.com>
2018-01-31 23:34:28 +0530

    new chanegs

--- Services/services_manage_webmaster/service_employee_details.php
+++ Services/services_manage_webmaster/service_employee_details.php
@@ -1,17 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from big-bang-studio.com/cosmos/pages-invoice.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Aug 2017 10:14:32 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="">
+    <title>Cosmos</title>
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/vendor.min.css">
+    <link rel="stylesheet" href="css/cosmos.min.css">
+    <link rel="stylesheet" href="css/application.min.css">
+  </head>
+
 <?php
 error_reporting(0);
 include_once('../../admin_includes/config.php');
 include_once('../../admin_includes/common_functions.php');
 
 $id = $_GET['id'];
-
 $getServiceEmployeeDetails = getIndividualDetails('services_employee_registration','id',$id);
-
 $getEmployeBelongsto = getIndividualDetails('service_employee_belongs_to','id',$getServiceEmployeeDetails['employee_belongs_to']);
 $employee_belongs_to = $getEmployeBelongsto['services_employee_belongs'];
-
 $img = $base_url . 'uploads/service_employee_photo/'.$getServiceEmployeeDetails['photo'];
-
 if($getServiceEmployeeDetails['employee_belongs_to'] == 1) {
 $service_provider = '--';
 } else {
@@ -19,72 +33,95 @@ $service_provider = '--';
   $service_provider = $serviceProvider['name'];
 }
 
-$content .='<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>SERVICE PROVIDER</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container" style="margin-top:20px;width:900px;">         
-  <table class="table" style="border:1px solid gray">
-    <thead>
-      <tr>
-        <th style="border-bottom:1px solid gray"><img src="'.$img.'" style="height:50px"></th>
-        <th colspan="2" style="border-bottom:1px solid gray"><h4 style="color:#f26226;text-align:center"><b>SERVICE EMPLOYEE DETAILS</b></h4></th>
-    <th style="border-bottom:1px solid gray"></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr  style="border-top:0px">    
-      <td style="border-bottom:1px solid gray;color:#f26226">Name:</td>
-    <td style="border-bottom:1px solid gray">'.$getServiceEmployeeDetails['name'].'</td>
-     <td style="border-bottom:1px solid gray;color:#f26226">Mobile:</td>
-    <td style="border-bottom:1px solid gray">'.$getServiceEmployeeDetails['mobile_number'].'</td>
-      </tr>
-      <tr>
-       <td style="border-bottom:1px solid gray;color:#f26226">Email</td>
-    <td style="border-bottom:1px solid gray">'.$getServiceEmployeeDetails['email'].'</td>
-    <td style="border-bottom:1px solid gray;color:#f26226">Created Date</td>
-    <td style="border-bottom:1px solid gray">'.$getServiceEmployeeDetails['created_at'].'</td>
-      </tr>
-    <tr>
-        <td style="border-bottom:1px solid gray;color:#f26226">Address</td>
-    <td colspan="3" style="border-bottom:1px solid gray">'.$getServiceEmployeeDetails['address'].'</td>
-      </tr>
-      <tr>
-       <td style="border-bottom:1px solid gray;color:#f26226">Experience</td>
-    <td style="border-bottom:1px solid gray">'.$getServiceEmployeeDetails['experience'].'</td>
-     <td style="border-bottom:1px solid gray;color:#f26226">Specalization</td>
-     <td style="border-bottom:1px solid gray">'.$getServiceEmployeeDetails['specalization'].'</td>
-      </tr>
-    <tr>
-        <td style="border-bottom:1px solid gray;color:#f26226">Employee Belongs to</td>
-     <td style="border-bottom:1px solid gray">'.$employee_belongs_to.'</td>
-     <td style="border-bottom:1px solid gray;color:#f26226">Service Provider</td>
-     <td style="border-bottom:1px solid gray">'.$service_provider.'</td>
-      </tr>
-    <tr>
-        <td style="color:#f26226">Description</td>
-    <td colspan="3">'.$getServiceEmployeeDetails['description'].'</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
-</body>
-</html>
-';
-
-//echo $content; die;
-
-require_once('html2pdf/html2pdf.class.php');
-
-
-$html2pdf = new HTML2PDF('P', array(450,250), 'en', true, 'UTF-8', array(30, 30, 40, 40));
-$html2pdf->pdf->SetDisplayMode('fullpage');
-$html2pdf->WriteHTML($content);
-$html2pdf->Output($getOrdersData1['order_id'].'.pdf');
 ?>
+  <body class="layout layout-header-fixed layout-left-sidebar-fixed">
+    <div class="site-overlay"></div>
+    
+    <div class="site-main">      
+     
+      <div class="site-content">
+        <div class="panel panel-default m-b-0">
+          <img src=<?php echo $img; ?> style="height:50px">
+          <div class="panel-heading">
+            <h3 class="m-y-0" style="text-align:center">SERVICE EMPLOYEE DETAILS</h3>
+          </div>
+          <div class="panel-body">
+           
+            <table class="table table-bordered m-b-30">
+              <thead>
+                <tr>
+                  <th>
+                    Name
+                  </th>
+                  <th>
+                    Mobile
+                  </th>
+                  <th>
+                    Email
+                  </th>
+                  <th>
+                    Created Date
+                  </th>                
+                  <th>
+                    Address
+                  </th>
+                  <th>
+                    Experience
+                  </th>
+                  <th>
+                    Specalization
+                  </th>
+                  <th>
+                    Employee Belongs to
+                  </th>
+                  <th>
+                    Service Provider
+                  </th>
+                  <th>
+                    Description
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>                
+                <tr>
+                  <td><?php echo $getServiceEmployeeDetails['name']; ?></td>
+                  <td><?php echo $getServiceEmployeeDetails['mobile_number']; ?></td>
+                  <td><?php echo $getServiceEmployeeDetails['email']; ?></td>
+                  <td><?php echo $getServiceEmployeeDetails['created_at']; ?></td>
+                  <td><?php echo $getServiceEmployeeDetails['address']; ?></td>                  
+                  <td><?php echo $getServiceEmployeeDetails['experience']; ?></td>
+                  <td><?php echo $getServiceEmployeeDetails['specalization']; ?></td>
+                  <td><?php echo $employee_belongs_to; ?></td>
+                  <td><?php echo $service_provider; ?></td>
+                  <td><?php echo $getServiceEmployeeDetails['description']; ?></td>                  
+                </tr>  
+              </tbody>
+            </table>
+          
+          </div>
+          <div class="panel-footer text-right">
+            <button type="button" class="btn btn-primary btn-labeled" onclick="myFunction()">Print
+              <span class="btn-label btn-label-right p-x-10">
+                <i class="zmdi zmdi-print"></i>
+              </span>
+            </button>
+            
+          </div>
+        </div>
+      </div>
+      <div class="site-footer">
+        2017 © LANCIUS IT SOLUTIONS
+      </div>
+    </div>
+    <script src="js/vendor.min.js"></script>
+    <script src="js/cosmos.min.js"></script>
+    <script src="js/application.min.js"></script>
+  </body>
+    <script>
+  function myFunction() {
+      window.print();
+  }
+  </script>
+
+</html>
