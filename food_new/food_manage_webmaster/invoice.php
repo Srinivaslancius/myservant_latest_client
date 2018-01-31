1adaf8df30c9df2f6a28e436d4f01e1f721028dc
Srinivaslancius <srinivas@lanciussolutions.com>
2018-02-01 00:06:08 +0530

    new invoice chnages

--- food_new/food_manage_webmaster/invoice.php
+++ food_new/food_manage_webmaster/invoice.php
@@ -1,6 +1,22 @@
 <!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from big-bang-studio.com/cosmos/pages-invoice.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Aug 2017 10:14:32 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="">
+    <title>Cosmos</title>
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/vendor.min.css">
+    <link rel="stylesheet" href="css/cosmos.min.css">
+    <link rel="stylesheet" href="css/application.min.css">
+  </head>
 
-<?php 
+<?php
 error_reporting(0);
 include_once('../../admin_includes/config.php');
 include_once('../../admin_includes/common_functions.php');
@@ -12,15 +28,10 @@ $getOrdersData = $conn->query($getOrders);
 $getOrdersData1 = $getOrdersData->fetch_assoc();
 
 $getSiteSettingsData = getIndividualDetails('food_site_settings','id',1);
-
 $getRestaurants = getIndividualDetails('food_vendors','id',$getOrdersData1['restaurant_id']);
-
 $getpaymentTypes = getIndividualDetails('lkp_payment_types','id',$getOrdersData1['payment_method']);
-
 $orderStatus = getIndividualDetails('lkp_food_order_status','id',$getOrdersData1['lkp_order_status_id']);
-
 $paymentStatus = getIndividualDetails('lkp_payment_status','id',$getOrdersData1['lkp_payment_status_id']);
-
 $getAddOnsPrice = "SELECT * FROM food_order_ingredients WHERE order_id = '$order_id'";
 $getAddontotal = $conn->query($getAddOnsPrice);
 $getAddontotalCount = $getAddontotal->num_rows;
@@ -28,9 +39,7 @@ $getAdstotal = 0;
 while($getAdTotal = $getAddontotal->fetch_assoc()) {
     $getAdstotal += $getAdTotal['item_ingredient_price'];
 }
-
 $service_tax = $getOrdersData1['sub_total']*$getSiteSettingsData['service_tax']/100;
-
 if($getOrdersData1['delivery_charges'] == '0') {
 	$order_type = "Take Away";
 	$delivery_charges = 0;
@@ -38,118 +47,144 @@ if($getOrdersData1['delivery_charges'] == '0') {
 	$order_type = "Delivery";
 	$delivery_charges = $getOrdersData1['delivery_charges'];
 }
+?>
+  <body class="layout layout-header-fixed layout-left-sidebar-fixed">
+    <div class="site-overlay"></div>
+    
+    <div class="site-main">      
+     
+      <div class="site-content">
+        <div class="panel panel-default m-b-0">
+          <div class="panel-heading">
+            <h3 class="m-y-0">Invoice</h3>
+          </div>
+          <div class="panel-body">
+            <div class="row m-b-30">
+              <div class="col-sm-6">                
+                <p><strong>Customer Address</strong></p>
+                <p><?php echo $getOrdersData1['first_name']; ?>
+                  <br><?php echo $getOrdersData1['email']; ?>
+                  <br><?php echo $getOrdersData1['address']; ?>,<?php echo $getOrdersData1['postal_code']; ?></p>
+                <p class="m-b-0">Mobile: <?php echo $getOrdersData1['mobile']; ?></p>
+              </div>
+              <div class="col-sm-6">                
+                <p><strong>Order Info</strong></p>
+                <p>Order Id: <?php echo $getOrdersData1['order_id']; ?>
+                  <br>Restaurant Name: <?php echo $getRestaurants['restaurant_name']; ?>
+                  <br>Order Date: <?php echo $getOrdersData1['created_at']; ?>
+                  <br>Order Status : <?php echo $orderStatus['order_status']; ?> 
+                  <br>Payment Status : <?php echo $paymentStatus['payment_status']; ?>
+                  <br>Payment Method: <?php echo $getpaymentTypes['status']; ?>
+              	</p>               
+              </div>
+            </div>
+            <table class="table table-bordered m-b-30">
+              <thead>
+                <tr>
+                  <th>
+                    S.No
+                  </th>
+                  <th>
+                    Item Name
+                  </th>
+                  <th>
+                    Menu Name
+                  </th>
+                  <th>
+                    Item Weight
+                  </th>                
+                  <th>
+                    Quantity
+                  </th>
+                  <th>
+                    Item Price
+                  </th>
+                  <th>
+                    Item Total
+                  </th>
+                </tr>
+              </thead>
 
- ?>
-<html lang="en">
-<head>
-  <title>Invoice</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container" style="margin-top:20px;width:1000px;">         
-  <table class="table" style="border:1px solid gray">
-    <thead>
-      <tr style="background-color:#f2f2f2">
-        <th colspan="2"></th>
-        <th colspan="2" style="padding-bottom:40px;padding-left:120px"><center><img src="<?php echo $base_url . 'uploads/food_logo/'.$getSiteSettingsData['logo'] ?>" class="logo-responsive" width="210px" height="100px;"></center></th>
-		<th></th>
-		<th colspan="2"><h3 style="color:#f26226">Invoice</h3>
-		<p>Oreder Id:<?php echo $getOrdersData1['order_id']; ?></p>
-		<p>Order Date:<?php echo $getOrdersData1['created_at']; ?></p>
-		</th>	
-      </tr>
-    </thead>
-    <tbody>
-      <tr>     
-        <td colspan="2"></td>
-        <td colspan="2" style="padding-left:150px"><center><h4 style="color:#f26226">ORDER DETAILS</h4></center></td>
-		<td colspan="3"></td>
-      </tr>
-      <tr  style="border-top:0px">
-	  
-       <td colspan="3"><p style="color:#f26226">Order Information</p>
-		<p>Restaurant Name: <?php echo $getRestaurants['restaurant_name']; ?></p>
-		<p>Payment Method: <?php echo $getpaymentTypes['status']; ?></p>
-		<p>Order Type: <?php echo $order_type; ?></p>
-		<p>Order Status: <?php echo $orderStatus['order_status']; ?></p>
-		<p>Payment Status: <?php echo $paymentStatus['payment_status']; ?></p>
-		</td>
-		
-        <td colspan="2"><p style="color:#f26226">Billing Address</p>
-		<p><?php echo $getOrdersData1['first_name']; ?></p>
-		<p><?php echo $getOrdersData1['email']; ?></p>
-		<p><?php echo $getOrdersData1['mobile']; ?></p>
-		<p><?php echo $getOrdersData1['address']; ?></p>
-		<p><?php echo $getOrdersData1['postal_code']; ?></p>
-		
-		</td>
-		
-        <td colspan="2"><p style="color:#f26226">Shipping Address</p>
-		<p><?php echo $getOrdersData1['first_name']; ?></p>
-		<p><?php echo $getOrdersData1['email']; ?></p>
-		<p><?php echo $getOrdersData1['mobile']; ?></p>
-		<p><?php echo $getOrdersData1['address']; ?></p>
-		<p><?php echo $getOrdersData1['postal_code']; ?></p></td>
-		
-      </tr>
-      <?php $getOrders1 = "SELECT * FROM food_orders WHERE order_id='$order_id'";
-		$getOrdersData3 = $conn->query($getOrders1); ?>
-      <tr style="color:#f26226">
-        <td colspan="2">PRODUCT NAME</td>
-        <td>CATEGORY NAME</td>
-        <td>ITEM WEIGHT</td>
-		<td>QUANTITY</td>
-		<td>PRICE</td>
-		<td></td>
-      </tr>
-      <?php while($getOrdersData2 = $getOrdersData3->fetch_assoc()) { 
-      	$getCategories = getIndividualDetails('food_category','id',$getOrdersData2['category_id']);
-      	$getProducts = getIndividualDetails('food_products','id',$getOrdersData2['product_id']);
-      	$getItemWeights = getIndividualDetails('food_product_weights','id',$getOrdersData2['item_weight_type_id']);
-      ?>
-	   <tr>
-        <td colspan="2"><?php echo $getProducts['product_name'] ?></td>
-        <td><?php echo  $getCategories['category_name']?></td>
-        <td><?php echo $getItemWeights['weight_type'] ?></td>
-		<td><?php echo $getOrdersData2['item_quantity'] ?></td>
-		<td><?php echo $getOrdersData2['item_price'] ?></td>
-		<td></td>
-      </tr>
-      <?php  } ?>
-	   <tr style="background-color:#f2f2f2">
-        <td colspan="5"></td>
-		<td>
-		<p>Subtotal:</p>
-		<p>Tax:</p>
-		<?php if($getOrdersData1['delivery_charges'] != '0') { ?>
-		<p>Delivery Charges:</p>
-		<?php } ?>
-		<?php if($getAddontotalCount > 0) { ?>
-		<p>Ingredients Price:</p>
-		<?php } ?>
-		<?php if($getOrdersData1['coupen_code'] != '') { ?>
-		<p>Discount:</p>
-		<?php } ?>
-		<p style="color:#f26226">Grand Total:</p>
-		</td>
-		<td style="color:#f26226"><p>Rs. <?php echo $getOrdersData1['sub_total']?></p>
-		<p>Rs. <?php echo $service_tax.'('.$getSiteSettingsData['service_tax'].'%)' ?></p>
-		<?php if($getOrdersData1['delivery_charges'] != '0') { ?>
-		<p>Rs. <?php echo $delivery_charges?></p>
-		<?php } ?>
-		<?php if($getAddontotalCount > 0) { ?>
-		<p>Rs. <?php echo $getAdstotal?></p>
-		<?php } ?>
-		<?php if($getOrdersData1['coupen_code'] != '') { ?>
-		<p>Rs. <?php echo $getOrdersData1['discout_money']?>(<span style="color:green">Coupon Applied.</span>)</p>
-		<?php } ?>
-		<p>Rs. <?php echo $getOrdersData1['order_total']?></p></td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+              <tbody>
+              	<?php 
+              		$i=1;
+	              	$getOrders1 = "SELECT * FROM food_orders WHERE order_id='$order_id'";
+					$getOrdersData3 = $conn->query($getOrders1);
+					while($getOrdersData2 = $getOrdersData3->fetch_assoc()) {
+					$getCategories = getIndividualDetails('food_category','id',$getOrdersData2['category_id']);
+			      	$getProducts = getIndividualDetails('food_products','id',$getOrdersData2['product_id']);
+			      	$getItemWeights = getIndividualDetails('food_product_weights','id',$getOrdersData2['item_weight_type_id']);					
+              	?>
+                <tr>
+                  <td><?php echo $i; ?></td>
+                  <td><?php echo $getProducts['product_name'] ?></td>
+                  <td><?php echo $getCategories['category_name'] ?></td>
+                  <td><?php echo $getItemWeights['weight_type']; ?></td>
+                  <td><?php echo $getOrdersData2['item_quantity']; ?></td>                  
+                  <td><?php echo $getOrdersData2['item_price']; ?></td>
+                  <td><?php echo $getOrdersData2['item_price']*$getOrdersData2['item_quantity']; ?></td>
+                </tr>  
+                <?php $i++; } ?>              
+                <tr>
+                  <td scope="row" colspan="6">
+                    <div class="text-right">
+                      Subtotal    
+                      <?php if($getAddontotalCount > 0) { ?>
+                      	<br> Ingredients Price:
+                      <?php } ?>   
+                      <?php if($getOrdersData1['delivery_charges'] != '0') { ?>
+                      	<br>Delivery Charges:
+                      <?php } ?> 
+                      <?php if($getOrdersData1['coupen_code'] != '') { ?>              
+                      	<br> Discount
+                      <?php } ?>
+                      <br> Service Tax
+                      <br>
+                      <strong>TOTAL</strong>
+                    </div>
+                  </td>
+                  <td>
+                    Rs .<?php echo $getOrdersData1['sub_total']; ?>
+                    <?php if($getAddontotalCount > 0) { ?>
+                    	<br>Rs. <?php echo $getAdstotal?>
+                    <?php } ?>
+                    <?php if($getOrdersData1['delivery_charges'] != '0') { ?>
+                    	<br>Rs. <?php echo $delivery_charges?>
+                    <?php } ?>
+                    <br> Rs .<?php echo $service_tax; ?> ( <?php echo $getSiteSettingsData['service_tax']; ?> % )
+                    <?php if($getOrdersData1['coupen_code'] != '') { ?>
+                    	<br>Rs .Rs. <?php echo $getOrdersData1['discout_money']?>(<span style="color:green">Coupon Applied.</span>)
+                    <?php } ?>                    
+                    <br>
+                    <strong>Rs .<?php echo $getOrdersData1['order_total']; ?></strong>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          
+          </div>
+          <div class="panel-footer text-right">
+            <button type="button" class="btn btn-primary btn-labeled" onclick="myFunction()">Print
+              <span class="btn-label btn-label-right p-x-10">
+                <i class="zmdi zmdi-print"></i>
+              </span>
+            </button>
+            
+          </div>
+        </div>
+      </div>
+      <div class="site-footer">
+        2017 © LANCIUS IT SOLUTIONS
+      </div>
+    </div>
+    <script src="js/vendor.min.js"></script>
+    <script src="js/cosmos.min.js"></script>
+    <script src="js/application.min.js"></script>
+  </body>
+  	<script>
+	function myFunction() {
+	    window.print();
+	}
+	</script>
 
-</body>
 </html>
