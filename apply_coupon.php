1e6b7ef14e41859852a57be4cca9c8ad6bd1eee8
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-05 17:07:41 +0530

    Changed coupons in admin panel

--- apply_coupon.php
+++ apply_coupon.php
@@ -7,37 +7,42 @@ if(!empty($_POST['coupon_code']) && !empty($_POST['cart_total']))  {
 	$coupon_code = $_POST['coupon_code'];
 	$cart_total = $_POST['cart_total'];
 	$getCouponType = getIndividualDetails('grocery_coupons','coupon_code',$coupon_code);
+	$orderCoupons1 = "SELECT * FROM grocery_orders WHERE coupen_code = '$coupon_code' AND user_id = '".$_SESSION['user_login_session_id']."'";
+	$orderCoupons = $conn->query($orderCoupons1);
 	if($getCouponType['coupon_type'] == 1) {
-		$sql="SELECT * FROM grocery_coupons WHERE coupon_code='$coupon_code' AND (now() BETWEEN coupon_start_date AND coupon_end_date) AND lkp_status_id = 0 AND category_id IN (SELECT category_id FROM grocery_cart WHERE user_id = '".$_SESSION['user_login_session_id']."') AND coupon_code NOT IN (SELECT coupen_code FROM grocery_orders WHERE user_id = '".$_SESSION['user_login_session_id']."')";
+		$sql="SELECT * FROM grocery_coupons WHERE coupon_code='$coupon_code' AND (now() BETWEEN coupon_start_date AND coupon_end_date) AND lkp_status_id = 0 AND category_id IN (SELECT category_id FROM grocery_cart WHERE user_id = '".$_SESSION['user_login_session_id']."')";
 	} elseif($getCouponType['coupon_type'] == 2) {
-		$sql="SELECT * FROM grocery_coupons WHERE coupon_code='$coupon_code' AND (now() BETWEEN coupon_start_date AND coupon_end_date) AND lkp_status_id = 0 AND sub_category_id IN (SELECT sub_category_id FROM grocery_cart WHERE user_id = '".$_SESSION['user_login_session_id']."') AND coupon_code NOT IN (SELECT coupen_code FROM grocery_orders WHERE user_id = '".$_SESSION['user_login_session_id']."')";
+		$sql="SELECT * FROM grocery_coupons WHERE coupon_code='$coupon_code' AND (now() BETWEEN coupon_start_date AND coupon_end_date) AND lkp_status_id = 0 AND sub_category_id IN (SELECT sub_category_id FROM grocery_cart WHERE user_id = '".$_SESSION['user_login_session_id']."')";
 	} elseif($getCouponType['coupon_type'] == 3) {
-		$sql="SELECT * FROM grocery_coupons WHERE coupon_code='$coupon_code' AND (now() BETWEEN coupon_start_date AND coupon_end_date) AND lkp_status_id = 0 AND coupon_code NOT IN (SELECT coupen_code FROM grocery_orders WHERE user_id = '".$_SESSION['user_login_session_id']."')";
+		$sql="SELECT * FROM grocery_coupons WHERE coupon_code='$coupon_code' AND (now() BETWEEN coupon_start_date AND coupon_end_date) AND lkp_status_id = 0";
 	}
 	$getCouponPrice = $conn->query($sql);
 	$getCouponPriceData = $getCouponPrice->fetch_assoc();
-	
-	if($getCouponPrice->num_rows > 0) {
-		if($getCouponPriceData['price_type_id'] == 1) {
-			$discount_price = $getCouponPriceData['discount_price'] * 1;
-			if($discount_price >= $cart_total) {
-				echo 1;
-			} else{
-				$cartTotal = ($cart_total - $discount_price);
-				echo $cartTotal.",".-$discount_price.",".$discount_price.",".$getCouponPriceData['price_type_id'];
-			}
-		} else {
-			$discount_price = ($cart_total/100) * $getCouponPriceData['discount_price'];
-			if($discount_price >= $cart_total) {
-				echo 1;
-			} else{
-				$cartTotal = ($cart_total - $discount_price);
-				echo $cartTotal.",".-$discount_price.",".$discount_price.",".$getCouponPriceData['price_type_id'];
-			}
-		}
-		
+	if($orderCoupons->num_rows > 0) {
+		echo 2;
 	} else {
-		echo 0;
+		if($getCouponPrice->num_rows > 0) {
+			if($getCouponPriceData['price_type_id'] == 1) {
+				$discount_price = $getCouponPriceData['discount_price'] * 1;
+				if($discount_price >= $cart_total) {
+					echo 1;
+				} else{
+					$cartTotal = ($cart_total - $discount_price);
+					echo $cartTotal.",".-$discount_price.",".$discount_price.",".$getCouponPriceData['price_type_id'];
+				}
+			} else {
+				$discount_price = ($cart_total/100) * $getCouponPriceData['discount_price'];
+				if($discount_price >= $cart_total) {
+					echo 1;
+				} else{
+					$cartTotal = ($cart_total - $discount_price);
+					echo $cartTotal.",".-$discount_price.",".$discount_price.",".$getCouponPriceData['price_type_id'];
+				}
+			}
+			
+		} else {
+			echo 0;
+		}
 	}
 }
 ?>
