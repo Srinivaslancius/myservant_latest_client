0ca82498381cc5edf863ade96b0ec729b0666b31
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-05 13:04:47 +0530

    Changed coupons in admin panel

--- grocery_admin/edit_grocery_coupons.php
+++ grocery_admin/edit_grocery_coupons.php
@@ -37,8 +37,17 @@
             $coupon_code = $_POST['coupon_code'];
             $price_type_id = $_POST['price_type_id'];
             $discount_price = $_POST['discount_price'];
-            $status = $_POST['lkp_status_id'];
-            $sql = "UPDATE `grocery_coupons` SET coupon_code=UPPER('$coupon_code'), price_type_id='$price_type_id', discount_price='$discount_price', lkp_status_id = '$status' WHERE id = '$id' ";
+            $coupon_type = $_POST['coupon_type'];
+            $coupon_start_date = date('y-m-d',strtotime($_POST['coupon_start_date']));
+            $coupon_end_date = date('y-m-d',strtotime($_POST['coupon_end_date']));
+            if($coupon_type == 1) {
+                $category_id = $_POST['category_id'];
+                $sub_category_id = '';
+            } elseif($coupon_type == 2) {
+                $category_id = '';
+                $sub_category_id = $_POST['sub_category_id'];
+            }
+            $sql = "UPDATE `grocery_coupons` SET coupon_code=UPPER('$coupon_code'), price_type_id='$price_type_id', discount_price='$discount_price', coupon_type='$coupon_type', coupon_start_date='$coupon_start_date', coupon_end_date='$coupon_end_date', category_id='$category_id', sub_category_id='$sub_category_id' WHERE id = '$id' ";
             if($conn->query($sql) === TRUE){
               echo "<script type='text/javascript'>window.location='grocery_coupons.php?msg=success'</script>";
             } else {
@@ -59,7 +68,46 @@
                             <div class="form-group">
                                 <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Coupon Code</label>
                                 <div class="col-sm-6 col-md-4">
-                                    <input type="text" style="text-transform:uppercase" name="coupon_code" class="form-control" id="form-control-3" placeholder="Enter Coupon Code" required value="<?php echo $getGroceryCouponsData['coupon_code'];?>">
+                                    <input type="text" style="text-transform:uppercase" name="coupon_code" class="form-control" id="user_input" onkeyup="checkUserAvailTest()"  placeholder="Enter Coupon Code" required value="<?php echo $getGroceryCouponsData['coupon_code'];?>">
+                                    <span id="input_status" style="color: red;"></span>
+                                    <input type="hidden" id="table_name" value="grocery_coupons">
+                                    <input type="hidden" id="column_name" value="coupon_code">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-3 col-md-4 control-label" for="form-control-9">Choose Coupon Type</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <select id="coupon_type" name="coupon_type" class="form-control" data-plugin="select2" data-options="{ theme: bootstrap }" required>
+                                        <option value="">Select Coupon Type</option>
+                                        <?php $getCouponTypes = getAllDataWithStatus('grocery_coupon_types','0');?>
+                                        <?php while($row = $getCouponTypes->fetch_assoc()) {  ?>
+                                            <option <?php if($row['id'] == $getGroceryCouponsData['coupon_type']) { echo "Selected"; } ?> value="<?php echo $row['id']; ?>" ><?php echo $row['coupon_type']; ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group" id="category">
+                                <label class="col-sm-3 col-md-4 control-label" for="form-control-9">Select Category</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <select id="form-control-1" name="category_id" class="form-control category" data-plugin="select2" data-options="{ theme: bootstrap }">
+                                        <option value="">-- Select Category --</option>
+                                        <?php $getCategories = getAllDataWithStatus('grocery_category','0');?>
+                                        <?php while($row = $getCategories->fetch_assoc()) {  ?>
+                                            <option <?php if($row['id'] == $getGroceryCouponsData['category_id']) { echo "Selected"; } ?> value="<?php echo $row['id']; ?>" ><?php echo $row['category_name']; ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group" id="sub_category">
+                                <label class="col-sm-3 col-md-4 control-label" for="form-control-9">Select Sub Category</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <select id="form-control-1" name="sub_category_id" class="form-control sub_category" data-plugin="select2" data-options="{ theme: bootstrap }">
+                                        <option value="">-- Select Sub Category --</option>
+                                        <?php $getSubacategories = getAllDataWithStatus('grocery_sub_category','0');?>
+                                        <?php while($row = $getSubacategories->fetch_assoc()) {  ?>
+                                            <option <?php if($row['id'] == $getGroceryCouponsData['sub_category_id']) { echo "Selected"; } ?> value="<?php echo $row['id']; ?>" ><?php echo $row['sub_category_name']; ?></option>
+                                        <?php } ?>
+                                    </select>
                                 </div>
                             </div>
                             <div class="form-group">
@@ -79,6 +127,18 @@
                                 </div>
                             </div>
                             <div class="form-group">
+                                <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Coupon Start Date</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <input class="date-pick form-control" data-format="yyyy-MM-dd" type="text" placeholder="Coupon Start Date" name="coupon_start_date" required="required" value="<?php echo $getGroceryCouponsData['coupon_start_date'];?>">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Coupon End Date</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <input class="date-pick form-control" data-format="yyyy-MM-dd" type="text" placeholder="Coupon End Date" name="coupon_end_date" required="required" value="<?php echo $getGroceryCouponsData['coupon_end_date'];?>">
+                                </div>
+                            </div>
+                            <div class="form-group">
                                 <div class="col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
                                     <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
                                 </div>
@@ -92,7 +152,40 @@
     <script src="js/dashboard-3.min.js"></script>
      <script src="js/forms-plugins.min.js"></script>
     <script src="js/tables-datatables.min.js"></script>
-
+    <script type="text/javascript">
+      $('input.date-pick').datepicker({minDate: 0, maxDate: "+2M"});
+    </script>
+    <script type="text/javascript">
+        $("#category,#sub_category").hide();
+        $(document).ready(function () {
+        $("#coupon_type").change(function() {
+            if($(this).val() == 1) {
+                $("#category").show();
+                $("#sub_category").hide();
+                $('.category').val("");
+                $(".category").attr("required", "true");
+                $(".sub_category").removeAttr('required');
+            } else if($(this).val() == 2) {
+                $("#sub_category").show();
+                $("#category").hide();
+                $('.sub_category').val("");
+                $(".sub_category").attr("required", "true");
+                $(".category").removeAttr('required');
+            } 
+        });
+        if($('#coupon_type').val() == 1) {
+            $("#category").show();
+            $("#sub_category").hide();
+            $(".category").attr("required", "true");
+            $(".sub_category").removeAttr('required');
+        } else if($('#coupon_type').val() == 2) {
+            $("#sub_category").show();
+            $("#category").hide();
+            $(".sub_category").attr("required", "true");
+            $(".category").removeAttr('required');
+        }
+      });
+    </script>
     
   </body>
 </html>
--- grocery_admin/grocery_coupons.php
+++ grocery_admin/grocery_coupons.php
@@ -36,7 +36,17 @@
           $coupon_code = $_POST['coupon_code'];
           $price_type_id = $_POST['price_type_id'];
           $discount_price = $_POST['discount_price'];
-          $sql = "INSERT INTO grocery_coupons (`coupon_code`, `price_type_id`, `discount_price`) VALUES (UPPER('$coupon_code'), '$price_type_id', '$discount_price')";
+          $coupon_type = $_POST['coupon_type'];
+          $coupon_start_date = date('y-m-d',strtotime($_POST['coupon_start_date']));
+          $coupon_end_date = date('y-m-d',strtotime($_POST['coupon_end_date']));
+          if($coupon_type == 1) {
+            $category_id = $_POST['category_id'];
+            $sub_category_id = '';
+          } elseif($coupon_type == 2) {
+            $category_id = '';
+            $sub_category_id = $_POST['sub_category_id'];
+          }
+          $sql = "INSERT INTO grocery_coupons (`coupon_code`, `price_type_id`, `discount_price`, `coupon_type`, `coupon_start_date`, `coupon_end_date`, `category_id`, `sub_category_id`) VALUES (UPPER('$coupon_code'), '$price_type_id', '$discount_price', '$coupon_type', '$coupon_start_date', '$coupon_end_date', '$category_id', '$sub_category_id')";
           if($conn->query($sql) === TRUE){
              echo "<script type='text/javascript'>window.location='grocery_coupons.php?msg=success'</script>";
           } else {
@@ -56,7 +66,46 @@
                             <div class="form-group">
                                 <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Coupon Code</label>
                                 <div class="col-sm-6 col-md-4">
-                                    <input type="text" style="text-transform:uppercase" name="coupon_code" class="form-control" id="form-control-3" placeholder="Enter Coupon Code" required>
+                                    <input type="text" style="text-transform:uppercase" name="coupon_code" class="form-control" id="user_input" onkeyup="checkUserAvailTest()" placeholder="Enter Coupon Code" required>
+                                    <span id="input_status" style="color: red;"></span>
+                                    <input type="hidden" id="table_name" value="grocery_coupons">
+                                    <input type="hidden" id="column_name" value="coupon_code">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-3 col-md-4 control-label" for="form-control-9">Choose Coupon Type</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <select id="coupon_type" name="coupon_type" class="form-control" data-plugin="select2" data-options="{ theme: bootstrap }" required>
+                                        <option value="">Select Coupon Type</option>
+                                        <?php $getCouponTypes = getAllDataWithStatus('grocery_coupon_types','0');?>
+                                        <?php while($row = $getCouponTypes->fetch_assoc()) {  ?>
+                                            <option value="<?php echo $row['id']; ?>" ><?php echo $row['coupon_type']; ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group" id="category">
+                                <label class="col-sm-3 col-md-4 control-label" for="form-control-9">Select Category</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <select id="form-control-1" name="category_id" class="form-control category" data-plugin="select2" data-options="{ theme: bootstrap }" required>
+                                        <option value="">-- Select Category --</option>
+                                        <?php $getCategories = getAllDataWithStatus('grocery_category','0');?>
+                                        <?php while($row = $getCategories->fetch_assoc()) {  ?>
+                                            <option value="<?php echo $row['id']; ?>" ><?php echo $row['category_name']; ?></option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group" id="sub_category">
+                                <label class="col-sm-3 col-md-4 control-label" for="form-control-9">Select Sub Category</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <select id="form-control-1" name="sub_category_id" class="form-control sub_category" data-plugin="select2" data-options="{ theme: bootstrap }" required>
+                                        <option value="">-- Select Sub Category --</option>
+                                        <?php $getSubacategories = getAllDataWithStatus('grocery_sub_category','0');?>
+                                        <?php while($row = $getSubacategories->fetch_assoc()) {  ?>
+                                            <option value="<?php echo $row['id']; ?>" ><?php echo $row['sub_category_name']; ?></option>
+                                        <?php } ?>
+                                    </select>
                                 </div>
                             </div>
                             <div class="form-group">
@@ -73,6 +122,18 @@
                                 <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Discount Price / Percentage</label>
                                 <div class="col-sm-6 col-md-4">
                                     <input type="text" name="discount_price" class="form-control valid_price_dec" id="form-control-2" placeholder="Discount Price / Percentage" data-error="Please enter Correct Discount Price / Percentage." required>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Coupon Start Date</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <input class="date-pick form-control" data-format="yyyy-MM-dd" type="text" placeholder="Coupon Start Date" name="coupon_start_date" required="required">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Coupon End Date</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <input class="date-pick form-control" data-format="yyyy-MM-dd" type="text" placeholder="Coupon End Date" name="coupon_end_date" required="required">
                                 </div>
                             </div>
                             <div class="form-group">
@@ -96,8 +157,13 @@
                                 <tr>
                                     <th>S.No</th>
                                     <th>Coupon Code</th>
+                                    <th>Coupon Type</th>
+                                    <th>Category</th>
+                                    <th>Sub Category</th>
                                     <th>Price Type</th>
                                     <th>Discount Price</th>
+                                    <th>Start Date</th>
+                                    <th>End Date</th>
                                     <th>Status</th>
                                     <th>Actions</th>
                                 </tr>
@@ -108,8 +174,13 @@
                                 <tr>
                                     <td><?php echo $i;?></td>
                                     <td><?php echo $row['coupon_code'];?></td>
+                                    <td><?php $getCouponType = getIndividualDetails('grocery_coupon_types','id',$row['coupon_type']); echo $getCouponType['coupon_type']; ?></td>
+                                    <td><?php if($row['category_id'] != '') { $getCategory = getIndividualDetails('grocery_category','id',$row['category_id']); echo $getCategory['category_name']; } else { echo '--' ; } ?></td>
+                                    <td><?php if($row['sub_category_id'] != '') { $getCategory = getIndividualDetails('grocery_sub_category','id',$row['sub_category_id']); echo $getCategory['sub_category_name']; } else { echo '--' ; } ?></td>
                                     <td><?php if($row['price_type_id'] == 1) { echo "Price"; } else { echo "Percentage"; }?></td>
                                     <td><?php echo $row['discount_price'];?></td>
+                                    <td><?php echo $row['coupon_start_date'];?></td>
+                                    <td><?php echo $row['coupon_end_date'];?></td>  
                                     <td><?php if ($row['lkp_status_id']==0) { echo "<span class='label label-outline-success check_active open_cursor' data-incId=".$row['id']." data-status=".$row['lkp_status_id']." data-tbname='grocery_coupons'>Active</span>" ;} else { echo "<span class='label label-outline-info check_active open_cursor' data-status=".$row['lkp_status_id']." data-incId=".$row['id']." data-tbname='grocery_coupons'>In Active</span>" ;} ?></td>
                                    <td> <a href="edit_grocery_coupons.php?coupon_id=<?php echo $row['id']; ?>"><i class="zmdi zmdi-edit"></i></a></td>
                                 </tr>
@@ -124,7 +195,28 @@
     <script src="js/dashboard-3.min.js"></script>
      <script src="js/forms-plugins.min.js"></script>
     <script src="js/tables-datatables.min.js"></script>
-
-    
+    <script type="text/javascript">
+      $('input.date-pick').datepicker({minDate: 0, maxDate: "+2M"});
+    </script>
+    <script type="text/javascript">
+        $("#category,#sub_category").hide();
+        $(document).ready(function () {
+        $("#coupon_type").change(function() {
+            if($(this).val() == 1) {
+                $("#category").show();
+                $("#sub_category").hide();
+                $('.category').val("");
+                $(".category").attr("required", "true");
+                $(".sub_category").removeAttr('required');
+            } else if($(this).val() == 2) {
+                $("#sub_category").show();
+                $("#category").hide();
+                $('.sub_category').val("");
+                $(".sub_category").attr("required", "true");
+                $(".category").removeAttr('required');
+            } 
+        });
+      });
+    </script>
   </body>
 </html>
