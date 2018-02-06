79ce87a2851b8265a64461db3418df3f4977095b
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-06 17:36:57 +0530

    added reorders in user dashboard

--- reorder.php
+++ reorder.php
@@ -31,7 +31,34 @@ while ($OrderDetails = $groceryOrdersData1->fetch_assoc()) {
 		$product_quantity = $getQun['product_quantity']+1;
 		$saveItems = "UPDATE grocery_cart SET product_quantity='$product_quantity' WHERE product_id='$product_id' AND product_weight_type = '$product_weight_type' AND session_cart_id='$session_cart_id'";
 	} else {
-		$saveItems = "INSERT INTO `grocery_cart`(`user_id`, `session_cart_id`, `category_id`, `sub_category_id`, `product_id`, `product_name`, `product_price`, `product_weight_type`,`product_quantity`, `lkp_city_id`, `created_at`,`device_id`,`reward_points`) VALUES ('$user_id','$session_cart_id','$category_id','$sub_category_id','$product_id','$product_name','$product_price','$product_weight_type','$product_quantity','$city_id','$created_at','$device_id','')";
+		$getRewardPointsdata = getIndividualDetails('grocery_reward_points','id',1);
+		if($getRewardPointsdata['reward_status'] == 0) {
+			$getProductRewards = "SELECT * FROM grocery_reward_settings WHERE product_id = '$product_id' AND reward_type = 4 AND lkp_status_id = 0";
+			$getAllProIds=$conn->query($getProductRewards);
+			$getRewd = $getAllProIds->fetch_assoc();
+			if($getAllProIds->num_rows > 0) {
+				$rewardPointsRewdSettings = ($product_price/$getRewardPointsdata['transaction_amount'])*$getRewd['reward_points'];
+			} else { 
+				$getSubcatRewards = "SELECT * FROM grocery_reward_settings WHERE sub_category_id = '$sub_category_id' AND product_id != '$product_id' AND reward_type = 3 AND lkp_status_id = 0";
+				$getAllSubcatIds=$conn->query($getSubcatRewards);
+				$getSubcatRewd = $getAllSubcatIds->fetch_assoc();
+				if($getAllSubcatIds->num_rows > 0) {
+					$rewardPointsRewdSettings = ($product_price/$getRewardPointsdata['transaction_amount'])*$getSubcatRewd['reward_points'];
+				} else {
+					$getCategoryRewards = "SELECT * FROM grocery_reward_settings WHERE category_id = '$category_id' AND sub_category_id != '$sub_category_id' AND product_id != '$product_id' AND reward_type = 2 AND lkp_status_id = 0";
+					$getAllCatIds=$conn->query($getCategoryRewards);
+					$getCatRewd = $getAllCatIds->fetch_assoc();
+					if($getAllCatIds->num_rows > 0) {
+						$rewardPointsRewdSettings = ($product_price/$getRewardPointsdata['transaction_amount'])*$getCatRewd['reward_points'];
+					} else {
+						$rewardPointsRewdSettings = ($product_price/$getRewardPointsdata['transaction_amount'])*$getRewardPointsdata['reward_points'];
+					}
+				}
+			}
+		} else {
+			$rewardPointsRewdSettings = 0;
+		}
+		$saveItems = "INSERT INTO `grocery_cart`(`user_id`, `session_cart_id`, `category_id`, `sub_category_id`, `product_id`, `product_name`, `product_price`, `product_weight_type`,`product_quantity`, `lkp_city_id`, `created_at`,`device_id`,`reward_points`) VALUES ('$user_id','$session_cart_id','$category_id','$sub_category_id','$product_id','$product_name','$product_price','$product_weight_type','$product_quantity','$city_id','$created_at','$device_id','$rewardPointsRewdSettings')";
 	}
 	$saveCart = $conn->query($saveItems);
 }
