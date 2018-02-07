df544238ddb03090d6fd60b71137527c3720c233
Srinivaslancius <srinivas@lanciussolutions.com>
2018-02-07 16:33:35 +0530

    notifcations

---
+++ FcmExample/index.php
@@ -0,0 +1,6 @@
+<form method="post" action="sendMultiplePush.php">
+	<input type="text" name="title" placeholder="Title">
+	<input type="text" name="message" placeholder="Message">	
+	<input type="text" name="image" value="https://lanciussolutions.com/FcmExample/download.png">
+	<input type="submit" name="submit" value="submit">
+</form>
---
+++ grocery_admin/mobile_push_notifications.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="">
+    <title>Cosmos</title>
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/vendor.min.css">
+    <link rel="stylesheet" href="css/cosmos.min.css">
+    <link rel="stylesheet" href="css/application.min.css">
+  </head>
+  <body class="layout layout-header-fixed layout-left-sidebar-fixed">
+    <div class="site-overlay"></div>
+    <div class="site-header">
+        <?php include_once './main_header.php';?>
+    </div>
+    <div class="site-main">
+      <div class="site-left-sidebar">
+        <div class="sidebar-backdrop"></div>
+        <div class="custom-scrollbar">
+            <?php include_once './side_menu.php';?>
+        </div>
+      </div>
+      <div class="site-right-sidebar">
+        <?php include_once './right_slide_toggle.php';?>
+      </div>
+      
+        <div class="site-content">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="m-y-0 font_sz_view">Send Mobile Notifications</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        
+                        <form class="form-horizontal" method="post" autocomplete="off" action="../FcmExample/sendMultiplePush.php">
+                            
+                            <div class="form-group">
+                                <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Title</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <input type="text" name="title" class="form-control" id="form-control-3" placeholder="Enter Title" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Message</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <input type="text" name="message" class="form-control" id="form-control-3" placeholder="Enter Message" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="form-control-3" class="col-sm-3 col-md-4 control-label">Image</label>
+                                <div class="col-sm-6 col-md-4">
+                                    <input type="text" name="image" class="form-control" id="form-control-3" placeholder="Image" value="https://lanciussolutions.com/FcmExample/download.png" >
+                                </div>
+                            </div>
+                           
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
+                                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+         
+        </div>
+        <?php include_once 'footer.php'; ?>
+    <script src="js/dashboard-3.min.js"></script>
+     <script src="js/forms-plugins.min.js"></script>
+    <script src="js/tables-datatables.min.js"></script>
+
+    
+  </body>
+</html>
