a9d2ba750da81c3f2664a84b564e8c0efcc62a3b
Srinivaslancius <srinivas@lanciussolutions.com>
2018-02-10 16:31:38 +0530

    done design changes

---
+++ add_address.php
@@ -0,0 +1,228 @@
+<?php include_once 'meta.php';?>
+<style>
+.order-tracking {
+	padding: 60px 90px 90px;
+}
+.order-tracking .title h3 {
+    color: #2d2d2d;
+    margin-bottom: 46px;
+}
+.btn-track1{
+	background-color: #FE6003;
+}
+</style>
+<body class="header_sticky">
+	<div class="boxed">
+
+		<div class="overlay"></div>
+
+		<!-- Preloader -->
+		<div class="preloader">
+			<div class="clear-loading loading-effect-2">
+				<span></span>
+			</div>
+		</div><!-- /.preloader -->
+		<section id="header" class="header">
+			<div class="header-top">
+			<?php include_once 'top_header.php';?>
+			</div><!-- /.header-top -->
+			<div class="header-middle">
+			<?php include_once 'middle_header.php';?>
+			</div><!-- /.header-middle -->
+			<div class="header-bottom">
+			<?php include_once 'bottom_header.php';?>
+			</div><!-- /.header-bottom -->
+		</section><!-- /#header -->
+		<section class="flat-breadcrumb">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="breadcrumbs">
+							<li class="trail-item">
+								<a href="index.php" title="">Home</a>
+								<span><img src="images/icons/arrow-right.png" alt=""></span>
+							</li>
+							<li class="trail-item">
+								<a href="#" title="">Track your orders</a>
+								
+							</li>
+							
+						</ul><!-- /.breacrumbs -->
+					</div><!-- /.col-md-12 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /.flat-breadcrumb -->
+
+		<section class="flat-tracking background">
+			<div class="container">
+				<div class="row">
+				
+					<div class="col-md-12">
+						<div class="order-tracking">
+							<div class="title">
+								<h3>Sample Heading</h3>
+								
+								<p class="subscibe">
+									Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br />Excepteur sint occaecat cupidatat non proident.
+								</p>
+							</div><!-- /.title -->
+							<div class="tracking-content">
+								<div class="row" style="margin-bottom:30px">
+								<div class="col-sm-2">
+								</div>
+								<div class="col-sm-5">
+								<div class="text" style="text-align:justify">
+								<label class="container3">
+								  <input type="radio" checked="checked" name="radio">Sample Heading
+								  <span class="checkmarkR1"></span>
+								</label>
+								<p>Name : swapna</p>
+								<p>Mobile No : 9876543210</p>
+								<p>Email : swapna@gmail.com</p>
+								<p>Place : Hyderabad</p>
+								</div>
+								</div>
+								<div class="col-sm-5">
+								<div class="text" style="text-align:justify">
+								<label class="container3">
+								  <input type="radio"  name="radio">Sample Heading
+								  <span class="checkmarkR1"></span>
+								</label>
+								<p>Name : swapna</p>
+								<p>Mobile No : 9876543210</p>
+								<p>Email : swapna@gmail.com</p>
+								<p>Place : Hyderabad</p>
+								</div>
+								</div>
+								
+								</div>
+								<div class="row">
+								<div class="col-sm-2">
+								</div>
+								<div class="col-sm-5">
+								<div class="text" style="text-align:justify">
+								<label class="container3">
+								  <input type="radio" checked="checked" name="radio">Sample Heading
+								  <span class="checkmarkR1"></span>
+								</label>
+								<p>Name : swapna</p>
+								<p>Mobile No : 9876543210</p>
+								<p>Email : swapna@gmail.com</p>
+								<p>Place : Hyderabad</p><br>
+								<div class="btn-track">
+										<button type="submit" class="btn-track1">Somthing</button>
+									</div>
+								</div>
+								</div>
+								<div class="col-sm-5">
+								<div class="text" style="text-align:justify">
+								<label class="container3">
+								  <input type="radio"  name="radio">Sample Heading
+								  <span class="checkmarkR1"></span>
+								</label>
+								<p>Name : swapna</p>
+								<p>Mobile No : 9876543210</p>
+								<p>Email : swapna@gmail.com</p>
+								<p>Place : Hyderabad</p>
+								</div>
+								</div>
+								
+								</div>
+							</div><!-- /.tracking-content -->
+						</div><!-- /.order-tracking -->
+					</div><!-- /.col-md-12 -->
+					
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /.flat-tracking -->
+
+		<section class="flat-row flat-iconbox style1 background">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="iconbox style1 v1">
+							<div class="box-header">
+								<div class="image">
+									<img src="images/icons/car.png" alt="">
+								</div>
+								<div class="box-title">
+									<h3>Free Shipping</h3>
+								</div>
+								<div class="clearfix"></div>
+							</div><!-- /.box-header -->
+						</div><!-- /.iconbox -->
+					</div><!-- /.col-md-3 -->
+					<div class="col-md-3">
+						<div class="iconbox style1 v1">
+							<div class="box-header">
+								<div class="image">
+									<img src="images/icons/order.png" alt="">
+								</div>
+								<div class="box-title">
+									<h3>Order Online Service</h3>
+								</div>
+								<div class="clearfix"></div>
+							</div><!-- /.box-header -->
+						</div><!-- /.iconbox -->
+					</div><!-- /.col-md-3 -->
+					<div class="col-md-3">
+						<div class="iconbox style1 v1">
+							<div class="box-header">
+								<div class="image">
+									<img src="images/icons/payment.png" alt="">
+								</div>
+								<div class="box-title">
+									<h3>Payment</h3>
+								</div>
+								<div class="clearfix"></div>
+							</div><!-- /.box-header -->
+						</div><!-- /.iconbox -->
+					</div><!-- /.col-md-3 -->
+					<div class="col-md-3">
+						<div class="iconbox style1 v1">
+							<div class="box-header">
+								<div class="image">
+									<img src="images/icons/return.png" alt="">
+								</div>
+								<div class="box-title">
+									<h3>Return 30 Days</h3>
+								</div>
+								<div class="clearfix"></div>
+							</div><!-- /.box-header -->
+						</div><!-- /.iconbox -->
+					</div><!-- /.col-md-3 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /.flat-iconbox -->
+	<footer>
+			<?php include_once 'footer.php';?>
+		</footer><!-- /footer -->
+
+		<section class="footer-bottom">
+			<?php include_once 'footer_bottom.php';?>
+		</section><!-- /.footer-bottom -->
+
+
+	</div><!-- /.boxed -->
+
+		<!-- Javascript -->
+		<script type="text/javascript" src="javascript/jquery.min.js"></script>
+		<script type="text/javascript" src="javascript/tether.min.js"></script>
+		<script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+		<script type="text/javascript" src="javascript/waypoints.min.js"></script>
+		<script type="text/javascript" src="javascript/jquery.circlechart.js"></script>
+		<script type="text/javascript" src="javascript/easing.js"></script>
+		<script type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
+		<script type="text/javascript" src="javascript/owl.carousel.js"></script>
+		<script type="text/javascript" src="javascript/smoothscroll.js"></script>
+		<script type="text/javascript" src="javascript/jquery-ui.js"></script>
+		<script type="text/javascript" src="javascript/jquery.mCustomScrollbar.js"></script>
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtRmXKclfDp20TvfQnpgXSDPjut14x5wk&region=GB"></script>
+	   	<script type="text/javascript" src="javascript/gmap3.min.js"></script>
+	   	<script type="text/javascript" src="javascript/waves.min.js"></script>
+		<script type="text/javascript" src="javascript/jquery.countdown.js"></script>
+
+		<script type="text/javascript" src="javascript/main.js"></script>
+
+</body>	
+</html>
