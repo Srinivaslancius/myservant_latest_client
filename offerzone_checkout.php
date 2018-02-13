ff7e6b2e758e240925761e356f2f077a808b78ff
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-13 12:55:24 +0530

    changed mail issues

--- offerzone_checkout.php
+++ offerzone_checkout.php
@@ -141,22 +141,22 @@
 				$dataem = $_POST["user_email"];
 				//$to = "srinivas@lanciussolutions.com";
 				$to = $dataem;
-				$from = $getSiteSettings1["orders_email"];
+				$from = $getSiteSettingsData1["orders_email"];
 				$subject = "My Servent - Offer Coupon ";
 				$message = '';
 				$message .= '<body>
 					<div class="container" style=" width:50%;border: 5px solid #fe6003;margin:0 auto">
 					<header style="padding:0.8em;color: white;background-color: #fe6003;clear: left;text-align: center;">
-					 <center><img src='.$base_url . "grocery_admin/uploads/logo/".$getSiteSettings1["logo"].' class="logo-responsive"></center>
+					 <center><img src='.$base_url . "grocery_admin/uploads/logo/".$getSiteSettingsData1["logo"].' class="logo-responsive"></center>
 					</header>
 					<article style=" border-left: 1px solid gray;overflow: hidden;text-align:justify; word-spacing:0.1px;line-height:25px;padding:15px">
 					  <h1 style="color:#fe6003">Greetings From Myservant</h1>
-					  <p>Dear <span style="color:#fe6003;">'.$first_name.'</span>, Thank you for Purchasing Coupons.  myservant.com!</p>
+					  <p>Dear <span style="color:#fe6003;">'.$user_first_name.'</span>, Thank you for Purchasing Coupons.  myservant.com!</p>
 						<p>Your Offer Coupon Code is: <span style="color:#fe6003;">'.$offer_code.'</span></p>
 						<p>We hope you enjoy your stay at myservant.com, if you have any problems, questions, opinions, praise, comments, suggestions, please free to contact us at any time.</p>
 						<p>Warm Regards,<br>The Myservant Team </p>
 					</article>
-					<footer style="padding: 1em;color: white;background-color: #fe6003;clear: left;text-align: center;">'.$getSiteSettings1['footer_text'].'</footer>
+					<footer style="padding: 1em;color: white;background-color: #fe6003;clear: left;text-align: center;">'.$getSiteSettingsData1['footer_text'].'</footer>
 					</div>
 
 					</body>';
