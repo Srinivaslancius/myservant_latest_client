b3ef7dc1a4a4977638d1067000bb167da2fdb352
Srinivaslancius <srinivas@lanciussolutions.com>
2018-02-14 12:14:51 +0530

    done design changes

--- total_products_grid.php
+++ total_products_grid.php
@@ -85,12 +85,12 @@ echo'<input type="hidden" id="cat_id1_'.$getProductsData1['id'].'" value="'.$get
                     echo'<a  class="wishlist" onClick="addWishList1('.$getProductsData1['id'].')" href="javascript:void(0)" >';
                     }
                         if(!isset($_SESSION['user_login_session_id'])) {
-                            echo'<img src="images/icons/wishlist.png" alt="">Wishlist';
+                            echo'<img src="images/icons/wishlist.png" alt=""> Wishlist';
                         } else { 
                             if($getCountWishLsit1 == 0) {
-                                echo'<img src="images/icons/wishlist.png" id="change_wishlist_img1_'.$getProductsData1['id'].'" alt="">Wishlist';
+                                echo'<img src="images/icons/wishlist.png" id="change_wishlist_img1_'.$getProductsData1['id'].'" alt=""> Wishlist';
                             } else { 
-                                echo'<img src="images/icons/1.png" alt="" id="change_wishlist_img1_'.$getProductsData1['id'].'">Wishlist';
+                                echo'<img src="images/icons/1.png" alt="" id="change_wishlist_img1_'.$getProductsData1['id'].'"> Wishlist';
                             }
                         }
                     echo'</a>
