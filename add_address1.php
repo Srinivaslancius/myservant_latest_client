ca8d19437213f87e425c02f83b8e08c228b6a28a
Srinivaslancius <srinivas@lanciussolutions.com>
2018-02-14 16:07:24 +0530

    new cjhnages

--- add_address1.php
+++ add_address1.php
@@ -429,13 +429,13 @@
 		</script>
 
 		<script type="text/javascript">
-			$( document ).ready(function() {       
-		        var radioValue = $("input[name='make_it_default']:checked").val();
-		        $('.checkout').click(function(){
-				   window.location.href='shop_checkout.php?adid= '+radioValue+'';
-				});
-		        //alert(radioValue);
-		    });
+			//$( document ).ready(function() {  
+	        $('.checkout').click(function(){
+	        	var radioValue = $("input[name='make_it_default']:checked").val();
+		        window.location.href='shop_checkout.php?adid='+radioValue+'';
+		        return false;
+			});
+		    //});
 		</script>
 
 	</body>	
