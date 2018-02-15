8d5c3fc39d7bb00df6c0f63459697278002e05b2
Srinivaslancius <srinivas@lanciussolutions.com>
2018-02-15 15:44:17 +0530

    new cjhnages

--- food_new/review_section.php
+++ food_new/review_section.php
@@ -272,11 +272,12 @@
 <script type="text/javascript" src="js/rating.js"></script>
 <script language="javascript" type="text/javascript">
 $(function() {
+  alert();
     $("#rating_star").spaceo_rating_widget({
         starLength: '5',
         initialValue: '',
         callbackFunctionName: 'processRating',
-        imageDirectory: 'img/',
+        imageDirectory: 'img',
         inputAttr: 'post_id'
     });
 });
