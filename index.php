3423f27c3469f9c5578ad84ab2ddb45c90b81eae
Srinivaslancius <srinivas@lanciussolutions.com>
2018-02-16 14:51:52 +0530

    done design changes

--- index.php
+++ index.php
@@ -1062,8 +1062,8 @@ if($getTodayDeals1->num_rows > 0) { ?>
 			      success:function(response) {
 			      	//window.location.href = "shop_cart.php";
 			      	var x = document.getElementById("cart_popup_"+ProductId);
-				    x.className = "snackbar show";				    
-				    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
+				    x.className = "snackbar show1";				    
+				    setTimeout(function(){ x.className = x.className.replace("show1", ""); }, 1000);
 			      }
 			    });
 			    $.ajax({
