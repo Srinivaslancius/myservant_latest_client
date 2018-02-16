c2348e6f76f85f25df673e6c6a428d8c470595e2
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-16 19:22:25 +0530

    changed food issues

--- food_new/apply_coupon.php
+++ food_new/apply_coupon.php
@@ -9,28 +9,35 @@ if(!empty($_POST['coupon_code']) && !empty($_POST['cart_total']))  {
 	$sql="SELECT * FROM food_coupons WHERE coupon_code='$coupon_code' AND lkp_status_id = 0";
 	$getCouponPrice = $conn->query($sql);
 	$getCouponPriceData = $getCouponPrice->fetch_assoc();
+
+	//Checking coupon already used or not
+	$orderCoupons1 = "SELECT * FROM food_orders WHERE coupen_code = '$coupon_code' AND user_id = '".$_SESSION['user_login_session_id']."'";
+	$orderCoupons = $conn->query($orderCoupons1);
 	
-	if($getCouponPrice->num_rows > 0) {
-		if($getCouponPriceData['price_type_id'] == 1) {
-			$discount_price = $getCouponPriceData['discount_price'] * 1;
-			if($discount_price >= $cart_total) {
-				echo 1;
-			} else{
-				$cartTotal = ($cart_total - $discount_price);
-				echo $cartTotal.",".-$discount_price.",".$discount_price.",".$getCouponPriceData['price_type_id'];
+	if($orderCoupons->num_rows > 0) {
+		echo 2;
+	} else {
+		if($getCouponPrice->num_rows > 0) {
+			if($getCouponPriceData['price_type_id'] == 1) {
+				$discount_price = $getCouponPriceData['discount_price'] * 1;
+				if($discount_price >= $cart_total) {
+					echo 1;
+				} else{
+					$cartTotal = ($cart_total - $discount_price);
+					echo $cartTotal.",".-$discount_price.",".$discount_price.",".$getCouponPriceData['price_type_id'];
+				}
+			} else {
+				$discount_price = ($cart_total/100) * $getCouponPriceData['discount_price'];
+				if($discount_price >= $cart_total) {
+					echo 1;
+				} else{
+					$cartTotal = ($cart_total - $discount_price);
+					echo $cartTotal.",".-$discount_price.",".$discount_price.",".$getCouponPriceData['price_type_id'];
+				}
 			}
 		} else {
-			$discount_price = ($cart_total/100) * $getCouponPriceData['discount_price'];
-			if($discount_price >= $cart_total) {
-				echo 1;
-			} else{
-				$cartTotal = ($cart_total - $discount_price);
-				echo $cartTotal.",".-$discount_price.",".$discount_price.",".$getCouponPriceData['price_type_id'];
-			}
+			echo 0;
 		}
-		
-	} else {
-		echo 0;
 	}
 }
 ?>
