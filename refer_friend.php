978a0f675453cb764a42dedf4afaca3c50303bea
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-16 17:38:31 +0530

    changes in footer and refer a friend

--- refer_friend.php
+++ refer_friend.php
@@ -50,7 +50,58 @@
 				</div><!-- /.row -->
 			</div><!-- /.container -->
 		</section><!-- /.flat-breadcrumb -->
+<?php $getSiteSettings1 = getAllDataWhere('grocery_site_settings','id','1'); 
+$getSiteSettingsData1 = $getSiteSettings1->fetch_assoc(); ?>
+<?php 
+if(isset($_POST['refer_friend']) && !empty($_POST['refer_friend'])) {
+	
+	//echo "<pre>"; print_r($_POST); die;
+	$refer_email = $_POST['refer_email'];
+	$user_id = $_SESSION['user_login_session_id'];
+	$string2 = str_shuffle('1234567890');
+	$referal_code = substr($string2,0,5);
+	$getEmail1 = "SELECT * FROM users WHERE user_email LIKE '$refer_email'";
+	$getEmail = $conn->query($getEmail1);
+	$getEmailDeatils = $getEmail->fetch_assoc();
+	$created_at = date('Y-m-d H:i:s');
+	if($getEmail->num_rows == 0) {
+		$sql = "INSERT INTO grocery_refer_a_friend (`refered_user_id`,`refer_email_id`,`created_at`,`referal_code`) VALUES ('$user_id','$refer_email','$created_at','$referal_code')";
+  		$result = $conn->query($sql);
+  		if($result === TRUE) {
+			$to = $refer_email;
+			$subject = "Myservent - Refer a Friend";
+			$message = '';		
+			$message .= '<body>
+				<div class="container" style=" width:50%;border: 5px solid #fe6003;margin:0 auto">
+				<header style="padding:0.8em;color: white;background-color: #fe6003;clear: left;text-align: center;">
+				 <center><img src='.$base_url . "grocery_admin/uploads/logo/".$getSiteSettingsData1["logo"].' class="logo-responsive"></center>
+				</header>
+				<article style=" border-left: 1px solid gray;overflow: hidden;text-align:justify; word-spacing:0.1px;line-height:25px;padding:15px">
+				  	<h1 style="color:#fe6003">Welcome To Myservant</h1>
+			  		<p>A very special welcome to you <span style="color:#fe6003;">'.$refer_email.'</span></p>
+			  		<p>Your Friend <span style="color:#fe6003;">'.$getEmailDeatils['user_full_name'].' has reffered.</span></p>
+			  		<p>Your Referal code <span style="color:#fe6003;">'.$referal_code.'.</span></p>
+					<p>We hope you enjoy your stay at myservant.com, if you have any problems, questions, opinions, praise, comments, suggestions, please free to contact us at any time.</p>
+					<p>Warm Regards,<br>The Myservant Team </p>
+				</article>
+				<footer style="padding: 1em;color: white;background-color: #fe6003;clear: left;text-align: center;">'.$getSiteSettingsData1['footer_text'].'</footer>
+				</div>
 
+				</body>';
+
+			//echo $message; die;
+			$name = "My Servant - Grocery";
+			$from = $getSiteSettingsData1["from_email"];
+			$resultEmail = sendEmail($to,$subject,$message,$from,$name);
+			echo "<script>alert('Thank You. Your recommendation has been sent to $refer_email.');</script>";
+		} else {
+			echo "<script>alert('Sorry! There was a problem sending your recommendation.');</script>";
+		}
+	} else {
+		echo "<script>alert('Sorry! You Cant refered this Mail.');</script>";
+	}
+}
+?>
 		<section class="flat-tracking background">
 			<div class="container">
 				<div class="row">
@@ -60,6 +111,7 @@
 								<h2 style="color:#fe6003">Refer Our Services And Earn cash..!!</h2>
 								
 							</div>
+							<form method="POST">
 							<div class="tracking-content">
 								<img src="images/refer-a-friend.png">
 								<p>Do you want to make some extra money? Tell your family and friends<br>about us and start making extra cash!</p><br>
@@ -68,16 +120,17 @@
 								</div>
 								<div class="col-sm-6">
 								<div class="form-group">						
-						<input type="text" class="form-control"  name="user_full_name" placeholder="Enter a valid email address" required>
+						<input type="email" class="form-control" name="refer_email" placeholder="please enter email"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
 			            </div>
 						</div>
 						<div class="col-sm-3">
 								</div>
 						</div>
 					<div class="form-group">
-						<button class="button1" type="submit" name="save" style="font-size:16px;padding:0px 40px">Send Invite</button> 					
+						<button class="button1" type="submit" name="refer_friend" value="refer_friend" style="font-size:16px;padding:0px 40px">Send Invite</button> 					
 					</div>	
 							</div><!-- /.tracking-content -->
+						</form>
 						</div><!-- /.order-tracking -->
 					</div><!-- /.col-md-12 -->
 				</div><!-- /.row -->
