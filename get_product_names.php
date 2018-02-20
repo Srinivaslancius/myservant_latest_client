d57101c346a3bb15656d14b80db4e8a6e81e4586
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-20 10:35:26 +0530

    changed to display names

--- get_product_names.php
+++ get_product_names.php
@@ -10,7 +10,7 @@ if($_SESSION['city_name'] == '') {
 if (isset($_POST['keyword']) && $_POST['keyword']!=''){
     $states = array();
     $term = $_POST['keyword'];
-    $sql3 = "SELECT * FROM grocery_product_name_bind_languages WHERE search_tags LIKE '%$term%' AND product_id IN (SELECT id FROM grocery_products WHERE `lkp_status_id`= 0 AND id IN (SELECT product_id FROM grocery_product_bind_weight_prices WHERE lkp_status_id = 0 AND lkp_city_id = $lkp_city_id)) ORDER BY id DESC";
+    $sql3 = "SELECT * FROM grocery_product_name_bind_languages WHERE search_tags LIKE '%$term%' AND product_id IN (SELECT id FROM grocery_products WHERE `lkp_status_id`= 0 AND id IN (SELECT product_id FROM grocery_product_bind_weight_prices WHERE lkp_status_id = 0 AND lkp_city_id = $lkp_city_id)) GROUP BY product_id ORDER BY id DESC";
     $result = $conn->query($sql3);  
     if($result->num_rows > 0) {
 
