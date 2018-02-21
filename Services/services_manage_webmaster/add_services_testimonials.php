34915e74e959d95e46799f2cfc7abac715931f2b
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-21 15:21:24 +0530

    changed some issues

--- Services/services_manage_webmaster/add_services_testimonials.php
+++ Services/services_manage_webmaster/add_services_testimonials.php
@@ -9,17 +9,22 @@
       $phone_number = $_POST['phone_number'];
       $reason = $_POST['reason'];
       $description = $_POST['description'];
-      $created_at = date('Y-m-d H:i:s', time() + 24 * 60 * 60);
+      $created_at = date('Y-m-d H:i:s');
       $lkp_status_id = $_POST['lkp_status_id'];
       $fileToUpload = uniqid().$_FILES["fileToUpload"]["name"];
-      if($fileToUpload!='') {
+      $app_image = uniqid().$_FILES["app_image"]["name"];
+      if($fileToUpload!='' && $app_image!='') {
 
         $target_dir = "../../uploads/services_testimonials_images/";
         $target_file = $target_dir . basename($fileToUpload);
         $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-             $sql = "INSERT INTO services_testimonials (`title`,`email`,`phone_number`,`reason`, `description`, `image`, `lkp_status_id`, `created_at`) VALUES ('$title','$email','$phone_number','$reason', '$description','$fileToUpload', 1, '$created_at')";
+        $target_dir1 = "../../uploads/services_testimonials_app_images/";
+        $target_file1 = $target_dir1 . basename($app_image);
+        $imageFileType1 = pathinfo($target_file1,PATHINFO_EXTENSION);
+
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file) && move_uploaded_file($_FILES["app_image"]["tmp_name"], $target_file1)) {
+             $sql = "INSERT INTO services_testimonials (`title`,`email`,`phone_number`,`reason`, `description`, `image`,`app_image`, `lkp_status_id`, `created_at`) VALUES ('$title','$email','$phone_number','$reason', '$description','$fileToUpload','$app_image', 1, '$created_at')";
             if($conn->query($sql) === TRUE){
                echo "<script type='text/javascript'>window.location='services_testimonials.php?msg=success'</script>";
             } else {
@@ -72,7 +77,7 @@
                   </div>
 
                   <div class="form-group">
-                    <label for="form-control-4" class="control-label">Image</label>
+                    <label for="form-control-4" class="control-label">Web Image</label>
                     <img id="output" height="100" width="100"/>
                     <label class="btn btn-default file-upload-btn">
                       Choose file...
@@ -80,11 +85,19 @@
                       </label> (width : 75px ; height : 75px)
                   </div>
                   <div class="form-group">
+                    <label for="form-control-4" class="control-label">App Image</label>
+                    <img id="output1" height="100" width="100"/>
+                    <label class="btn btn-default file-upload-btn">
+                      Choose file...
+                        <input id="form-control-22" class="file-upload-input" type="file" accept="image/*" name="app_image" id="fileToUpload"  onchange="loadFile1(event)"  multiple="multiple" required>
+                      </label> (width : 75px ; height : 75px)
+                  </div>
+                  <div class="form-group">
                     <label for="form-control-2" class="control-label">Description</label>
                     <textarea name="description" class="form-control" id="category_description" placeholder="Group Description" data-error="Please enter Description." required></textarea>
                     <div class="help-block with-errors"></div>
                   </div>
-                  <?php $getStatus = getAllData('lkp_status');?>
+                  <!-- <?php $getStatus = getAllData('lkp_status');?>
                   <div class="form-group">
                     <label for="form-control-3" class="control-label">Choose your status</label>
                     <select id="form-control-3" name="lkp_status_id" class="custom-select" data-error="This field is required." required>
@@ -94,7 +107,7 @@
                       <?php } ?>
                    </select>
                     <div class="help-block with-errors"></div>
-                  </div>
+                  </div> -->
                   <button type="submit" name="submit" class="btn btn-primary btn-block">Submit</button>
                 </form>
               </div>
