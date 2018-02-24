5d82ac00ad980c1a3e5ec2e5802e4905fd030287
Srinivaslancius <srinivas@lanciussolutions.com>
2018-02-24 12:35:33 +0530

    new chanegs in sms and email

--- gplus_login/settings.php
+++ gplus_login/settings.php
@@ -1,13 +1,13 @@
 <?php
 
 /* Google App Client Id */
-define('CLIENT_ID', '373078096746-16mpbeb98rlq8cvvevpc2sbpgkjsbdf9.apps.googleusercontent.com');
+define('CLIENT_ID', '426266771557-pn758o9tgqsdr0k6a10gb7m851rue4uu.apps.googleusercontent.com');
 
 /* Google App Client Secret */
-define('CLIENT_SECRET', '7AAlHAlFkzM_xynDW36flijU');
+define('CLIENT_SECRET', 'HNZKW4PI0GCTJBd0kaCfiOCx');
 
 /* Google App Redirect Url */
-define('CLIENT_REDIRECT_URL', 'https://onlyprojects.info/gplus_login/gauth.php');
+define('CLIENT_REDIRECT_URL', 'https://myservant.com/gplus_login/gauth.php');
 
 ?>
 
