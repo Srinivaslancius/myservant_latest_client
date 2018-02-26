a8ee7a2e7fe59d7b0367d88cbf659882161f083c
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-26 15:42:09 +0530

    Include Sub-Location in MyAddresses - But it's value is not mandatory

--- shop_checkout.php
+++ shop_checkout.php
@@ -143,6 +143,12 @@ th,td{
 				$delivery_date = date("Y-m-d",strtotime($_POST["delivery_slot_date"]));
 				$delivery_time = $_POST["delivery_time"];
 
+				if($_POST['lkp_sub_area_id'] == 0) {
+					$lkp_sub_area_id = '';
+				} else {
+					$lkp_sub_area_id = $_POST['lkp_sub_area_id'];
+				}
+
 				$order_total = $_POST['order_total'];
 				if($_SESSION['CART_TEMP_RANDOM'] == "") {
 			        $_SESSION['CART_TEMP_RANDOM'] = rand(10, 10).sha1(crypt(time())).time();
@@ -169,10 +175,9 @@ th,td{
 					$date = date("ymdhis");
 					$contstr = "MYSERVANT-GR";
 					$sub_order_id = $contstr.$random1.$random2.$date;
-					$orders = "INSERT INTO grocery_orders (`user_id`,`first_name`, `last_name`, `email`, `mobile`, `address`, `lkp_state_id`, `lkp_district_id`, `lkp_city_id`, `lkp_pincode_id`, `lkp_location_id`, `order_note`, `category_id`, `sub_cat_id`, `product_id`, `item_weight_type_id`, `item_price`, `item_quantity`, `sub_total`, `order_total`, `coupen_code`, `coupen_code_type`, `discout_money`,  `payment_method`,`lkp_payment_status_id`,`service_tax`,`delivery_charges`,`delivery_slot_date`,`delivery_time`, `order_id`,`order_sub_id`,`wallet_id`,`wallet_amount`, `created_at`, `reward_points`, `product_reward_points`, `coupon_id`, `coupon_device_type`) VALUES ('$user_id','".$_POST["first_name"]."','".$_POST["last_name"]."', '".$_POST["email"]."','".$_POST["mobile"]."','".$_POST["address"]."','".$_POST["lkp_state_id"]."','".$_POST["lkp_district_id"]."','".$_POST["lkp_city_id"]."','".$_POST["lkp_pincode_id"]."','".$_POST["lkp_area_id"]."','".$_POST["order_note"]."','" . $_POST["category_id"][$i] . "','" . $_POST["sub_cat_id"][$i] . "','" . $_POST["product_id"][$i] . "','".$_POST['product_weight'][$i]."','" . $_POST["product_price"][$i] . "','" . $_POST["product_quantity"][$i] . "','".$_POST["sub_total"]."','".$_POST["order_total"]."',UPPER('$coupon_code'),'$coupon_code_type','$discount_money','$payment_group','$payment_status','".$_POST["service_tax"]."','$delivery_charges','$delivery_date','$delivery_time', '$order_id','$sub_order_id','$wallet_id','$wallet_amount','$order_date','$reward_points','" . $_POST["product_reward_points"][$i] . "','$coupon_id','$coupon_device_type')";
+					$orders = "INSERT INTO grocery_orders (`user_id`,`first_name`, `last_name`, `email`, `mobile`, `address`, `lkp_state_id`, `lkp_district_id`, `lkp_city_id`, `lkp_pincode_id`, `lkp_location_id`, `lkp_sub_location_id`, `order_note`, `category_id`, `sub_cat_id`, `product_id`, `item_weight_type_id`, `item_price`, `item_quantity`, `sub_total`, `order_total`, `coupen_code`, `coupen_code_type`, `discout_money`,  `payment_method`,`lkp_payment_status_id`,`service_tax`,`delivery_charges`,`delivery_slot_date`,`delivery_time`, `order_id`,`order_sub_id`,`wallet_id`,`wallet_amount`, `created_at`, `reward_points`, `product_reward_points`, `coupon_id`, `coupon_device_type`) VALUES ('$user_id','".$_POST["first_name"]."','".$_POST["last_name"]."', '".$_POST["email"]."','".$_POST["mobile"]."','".$_POST["address"]."','".$_POST["lkp_state_id"]."','".$_POST["lkp_district_id"]."','".$_POST["lkp_city_id"]."','".$_POST["lkp_pincode_id"]."','".$_POST["lkp_area_id"]."','$lkp_sub_area_id','".$_POST["order_note"]."','" . $_POST["category_id"][$i] . "','" . $_POST["sub_cat_id"][$i] . "','" . $_POST["product_id"][$i] . "','".$_POST['product_weight'][$i]."','" . $_POST["product_price"][$i] . "','" . $_POST["product_quantity"][$i] . "','".$_POST["sub_total"]."','".$_POST["order_total"]."',UPPER('$coupon_code'),'$coupon_code_type','$discount_money','$payment_group','$payment_status','".$_POST["service_tax"]."','$delivery_charges','$delivery_date','$delivery_time', '$order_id','$sub_order_id','$wallet_id','$wallet_amount','$order_date','$reward_points','" . $_POST["product_reward_points"][$i] . "','$coupon_id','$coupon_device_type')";
 					$groceryOrders = $conn->query($orders);
 				} 
-				
 				if($payment_group == 1) {
 					//cod 
 					header("Location: ordersuccess.php?odi=".$order_id."&pay_stau=2");				
@@ -218,6 +223,7 @@ th,td{
 									<input type="hidden" name="lkp_pincode_id" value="<?php echo $getCustomerDeatils['lkp_pincode_id']; ?>">
 									<input type="hidden" name="lkp_city_id" value="<?php echo $getCustomerDeatils['lkp_city_id']; ?>">
 									<input type="hidden" name="lkp_area_id" value="<?php echo $getCustomerDeatils['lkp_location_id']; ?>">
+									<input type="hidden" name="lkp_sub_area_id" value="<?php echo $getCustomerDeatils['lkp_sub_location_id']; ?>">
 									<div class="fields-title">
 										<h3>Billing details</h3>
 										<span></span>
@@ -273,6 +279,14 @@ th,td{
 												<label>Location *</label>
 												<input type="text" class="form-control" id="lkp_area_id" placeholder="Location" value="<?php echo $getArea['area_name']; ?>" readonly>
 											</p>
+											<?php 
+											if($getCustomerDeatils['lkp_sub_location_id'] != 0) {
+											$getsubArea = getIndividualDetails('grocery_lkp_sub_areas','id',$getCustomerDeatils['lkp_sub_location_id']); ?>
+											<p class="field-one-half">
+												<label>Sub Location</label>
+												<input type="text" class="form-control" id="lkp_area_id" placeholder="Location" value="<?php echo $getsubArea['sub_area_name']; ?>" readonly>
+											</p>
+											<?php } ?>
 											<div class="clearfix"></div>
 										</div>
 										<div class="field-row">
