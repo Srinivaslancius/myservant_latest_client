954f748cf14cf4e86a992a678105b87f0c03091c
srinivaslancius <srinivas@lanciussolutions.com>
2018-02-27 11:24:57 +0530

    changed forgot password page

--- Services/reset_password.php
+++ Services/reset_password.php
@@ -79,11 +79,9 @@
                 <div id="position">
 			<div class="container">
 				<ul>
-					<li><a href="#">Home</a>
+					<li><a href="index.php">Home</a>
 					</li>
-					<li><a href="#">Category</a>
-					</li>
-					<li>Page active</li>
+					<li>Reset Password</li>
 				</ul>
 			</div>
 		</div>
