ce2639c6560b8b3a74d81c00a0fa398686343f4a
srinivaslancius <srinivas@lanciussolutions.com>
2018-03-02 16:20:24 +0530

    changed login for facebook login

--- login.php
+++ login.php
@@ -124,7 +124,69 @@
 	    <?php }?>
 
 	    <?php
+	    //Convert JSON data into PHP variable
+		$userData = json_decode($_POST['userData']);
+		if(!empty($userData)){
+			$first_name = $userData->first_name;
+		    $getUserEmail = $userData->email;
+		    //Check whether user data already exists in database
+		    $getUserSelData =  "SELECT * FROM users WHERE user_email='$getUserEmail' AND lkp_status_id = 0";
+			$getLoginData = $conn->query($getUserSelData);
+			$getLoggedInDetails = $getLoginData->fetch_assoc();
+		    if($getLoginData->num_rows > 0){ 
+		        //Update user data if already exists
+		        $last_login_visit1 = date("Y-m-d h:i:s");
+		    	$login_count1 = $getLoggedInDetails['login_count']+1;
+		    	$sql = "UPDATE `users` SET login_count='$login_count1', last_login_visit='$last_login_visit1' WHERE user_email = '$getUserEmail' ";
+		    	$row = $conn->query($sql);
+		        $_SESSION['user_login_session_id'] =  $getLoggedInDetails['id'];
+		        $_SESSION['user_login_session_name'] = $getLoggedInDetails['user_full_name'];
+		        $_SESSION['user_login_session_email'] = $getLoggedInDetails['user_email'];
+		        $_SESSION['timestamp'] = time();
+		        header('Location: ../index.php');
+		    }else{
+		        //Insert user data
+		        $first_name = $userData->first_name;
+				$user_email = $userData->email;
+				$user_mobile1 = "";
+				$user_password1 = "";	
+				$lkp_status_id1 = 0; //0-active, 1- inactive
+				$login_count21 = 1;
+				$last_login_visit2 = date("Y-m-d h:i:s");
+				$lkp_register_device_type_id1=1; //1- web, 2- android, 3-ios
+				$user_login_type1 = 3; //1-Normal, 2-Facebook,3-googleplus
+				$user_register_service_id1 = 3;
+				$created_at1 = date("Y-m-d h:i:s");
 
+				//Save User Data
+				$saveUser = saveUser($first_name, $user_email, $user_mobile1, $user_password1,$lkp_status_id1,$login_count2,$last_login_visit2,$lkp_register_device_type_id1,$user_login_type1,'',$user_register_service_id1,$created_at1);
+				$getUserSelData =  "SELECT * FROM users WHERE user_email='$user_email' AND lkp_status_id = 0";
+				$getLoginData = $conn->query($getUserSelData);
+				$getLoggedInDetails = $getLoginData->fetch_assoc();
+				$_SESSION['user_login_session_id'] =  $getLoggedInDetails['id'];
+		        $_SESSION['user_login_session_name'] = $getLoggedInDetails['user_full_name'];
+		        $_SESSION['user_login_session_email'] = $getLoggedInDetails['user_email'];
+		        $_SESSION['timestamp'] = time();
+		        //Save wallet Data
+		        if($_SESSION['wallet_id'] == "") {
+					$string1 = str_shuffle('abcdefghijklmnopqrstuvwxyz');
+					$random1 = substr($string1,0,3);
+					$string2 = str_shuffle('1234567890');
+					$random2 = substr($string2,0,3);
+					$contstr = "MYSER-WALLET_";
+					$_SESSION['wallet_id'] = $contstr.$random1.$random2;
+				}
+		        $wallet_id = $_SESSION['wallet_id'];
+				$user_id = $_SESSION['user_login_session_id'];
+				$created_at = date("Y-m-d h:i:s");
+				$amount = 0;
+				$sqlInwallet = "INSERT INTO `user_wallet`(`wallet_id`, `user_id`, `amount`, `created_at`) VALUES ('$wallet_id','$user_id','$amount','$created_at')";
+				$sqlInwallet1 = $conn->query($sqlInwallet);
+
+		        header('Location: ../index.php');
+		    }
+		}
+		//gplus login
 		require_once('gplus_login/settings.php');
 		$login_url = 'https://accounts.google.com/o/oauth2/v2/auth?scope=' . urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/plus.me') . '&redirect_uri=' . urlencode(CLIENT_REDIRECT_URL) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online';
 		?>
@@ -142,7 +204,8 @@
 							<div class="col-md-1">
 							</div>
                             <div class="col-md-5 col-sm-6">
-                                <a href="" class="btn btn-primary fac_book" data-toggle="modal" data-target="#myModal1"><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a>
+                            	<a href="javascript:void(0);" onclick="fbLogin()" id="fbLink" class="btn btn-primary fac_book" data-toggle="modal"><i class="fa fa-facebook" aria-hidden="true"></i></span>Facebook </a>
+                                <!-- <a href="" class="btn btn-primary fac_book" data-toggle="modal" data-target="#myModal1"><i class="fa fa-facebook" aria-hidden="true"></i> Facebook</a> -->
                             </div>
 							<div class="modal fade" id="myModal1" role="dialog">
 								<div class="modal-dialog modal-sm">
@@ -403,6 +466,73 @@ $("#input_referral").hide();
      }
    });
  });
+</script>
+<div id="userData"></div>
+
+<script>
+window.fbAsyncInit = function() {
+    // FB JavaScript SDK configuration and setup
+    FB.init({
+      appId      : '154517418580869', // FB App ID
+      cookie     : true,  // enable cookies to allow the server to access the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.12' // use graph api version 2.8
+    });
+    
+    // Check whether the user already logged in
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            //display user data
+            getFbUserData();
+        }
+    });
+};
+
+// Load the JavaScript SDK asynchronously
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Facebook login with JavaScript SDK
+function fbLogin() {
+    FB.login(function (response) {
+        if (response.authResponse) {
+            // Get and display the user profile data
+            getFbUserData();
+        } else {
+            document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
+        }
+    }, {scope: 'email'});
+}
+
+// Fetch the user profile data from facebook
+function getFbUserData(){
+    FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture'},
+    function (response) {
+        document.getElementById('fbLink').setAttribute("onclick","fbLogout()");
+        document.getElementById('fbLink').innerHTML = 'Logout from Facebook';
+        document.getElementById('user_name').innerHTML = response.first_name;
+        saveUserData(response);
+    });
+}
+
+function saveUserData(userData){
+    $.post('login.php', {oauth_provider:'facebook',userData: JSON.stringify(userData)}, function(data){ return true; });
+}
+
+// Logout from facebook
+function fbLogout() {
+    FB.logout(function() {
+        document.getElementById('fbLink').setAttribute("onclick","fbLogin()");
+        document.getElementById('fbLink').innerHTML = '<img src="fblogin.png"/>';
+        document.getElementById('userData').innerHTML = '';
+        document.getElementById('user_name').innerHTML = 'You have successfully logout from Facebook.';
+    });
+}
 </script>
 </body>	
 </html>
