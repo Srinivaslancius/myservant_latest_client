aa7ff466b97b576790a61178df3ce299ff2322af
srinivaslancius <srinivas@lanciussolutions.com>
2018-03-08 15:33:20 +0530

    Changed Some issues

--- grocery_mobile_app/view_address_list.php
+++ grocery_mobile_app/view_address_list.php
@@ -11,10 +11,11 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     if(isset($_REQUEST['userId']) && !empty($_REQUEST['userId'])) {
 
         $result = getAllDataWhere('grocery_add_address','user_id',$_REQUEST['userId']);
+        $getAddressDetails = $result->fetch_assoc();
         
         if ($result->num_rows > 0) {
-                $response["lists"] = array();
-                while($getAddressDetails = $result->fetch_assoc()) {
+                // $response["lists"] = array();
+                // while($getAddressDetails = $result->fetch_assoc()) {
                     $getState = getIndividualDetails('grocery_lkp_states','id',$getAddressDetails['lkp_state_id']);
                     $getDistrict = getIndividualDetails('grocery_lkp_districts','id',$getAddressDetails['lkp_district_id']);
                     $getPincode = getIndividualDetails('grocery_lkp_pincodes','id',$getAddressDetails['lkp_pincode_id']);
@@ -27,15 +28,23 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
                         $subArea = '';
                     }
                     //Chedck the condioton for emptty or not        
-                    $lists = array();
+                    // $lists = array();
                     $lists["id"] = $getAddressDetails["id"];
                     $lists["user_id"] = $getAddressDetails["user_id"];     
-                    $lists["name"] = $getAddressDetails["first_name"].$getAddressDetails["last_name"];              
-                    $lists["address_details"] = $getState["state_name"] . ',' . $getDistrict["district_name"] .',' . $getCity["city_name"] .',' . $getArea["area_name"] .',' . $subArea .', ' . $getAddressDetails["address"].',' . $getPincode["pincode"];
+                    $lists["name"] = $getAddressDetails["first_name"].$getAddressDetails["last_name"];
+                    $lists["state_name"] = $getState["state_name"]; 
+                    $lists["district_name"] = $getDistrict["district_name"]; 
+                    $lists["city_name"] = $getCity["city_name"]; 
+                    $lists["area_name"] = $getArea["area_name"]; 
+                    $lists["address"] = $getAddressDetails["address"]; 
+                    $lists["pincode"] = $getPincode["pincode"]; 
+                    $lists["phone"] = $getAddressDetails["phone"];
+                    $lists["subArea"] = $subArea; 
+                    // $lists["address_details"] = $getState["state_name"] . ',' . $getDistrict["district_name"] .',' . $getCity["city_name"] .',' . $getArea["area_name"] .',' . $subArea .', ' . $getAddressDetails["address"].',' . $getPincode["pincode"];
                     $lists["status"] = $getAddressDetails["lkp_status_id"];
                     
-                    array_push($response["lists"], $lists);      
-                }
+                //     array_push($response["lists"], $lists);      
+                // }
                 $response["success"] = 0;
                 $response["message"] = "Success";               
         } else {
