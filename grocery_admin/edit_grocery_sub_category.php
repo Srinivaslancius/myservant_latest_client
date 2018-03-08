722724f72cb469c7ff00277411cd465a1e46ffa5
srinivaslancius <srinivas@lanciussolutions.com>
2018-03-08 11:38:49 +0530

    changed some issues

--- grocery_admin/edit_grocery_sub_category.php
+++ grocery_admin/edit_grocery_sub_category.php
@@ -68,7 +68,7 @@
                                 <label class="col-sm-3 control-label" for="form-control-9">Select Category</label>
                                 <div class="col-sm-6 col-md-4">
                                     <select id="form-control-1" name="grocery_category_id" class="form-control" data-plugin="select2" data-options="{ theme: bootstrap }" required required>
-                                      <option value="">--Select State--</option>
+                                      <option value="">--Select Category--</option>
                                       <?php while($row = $getCategories->fetch_assoc()) {  ?>
                                           <option <?php if($row['id'] == $getSubCategories['grocery_category_id']) { echo "Selected"; } ?> value="<?php echo $row['id']; ?>"><?php echo $row['category_name']; ?></option>
                                       <?php } ?>
