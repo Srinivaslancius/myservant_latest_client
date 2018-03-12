e5f2cd1ce532e55312d2107702ab4bd28e7b1155
srinivaslancius <srinivas@lanciussolutions.com>
2018-03-12 19:00:02 +0530

    Added service for offerzone checkout

--- grocery_mobile_app/offer_zone_orders.php
+++ grocery_mobile_app/offer_zone_orders.php
@@ -11,6 +11,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 		$offer_id = $_REQUEST['offerId'];
 		$getUserData = getIndividualDetails('users','id',$user_id);
 		$offerZone = getIndividualDetails('grocery_offer_zone','id',$id);
+		$getSiteSettingsData1 = getIndividualDetails('grocery_site_settings','id','1');
 
 		$user_first_name = $getUserData['user_full_name'];
 		$user_last_name = $getUserData['user_full_name'];
@@ -38,7 +39,39 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
             // check the conditions for query success or not
             $response["success"] = 0;            
             $response["message"] = "Save Successfully";   
-            $response["order_id"] = $order_id;      
+            $response["order_id"] = $order_id;
+
+            $dataem = $_POST["user_email"];
+			$to = $dataem;
+			$from = $getSiteSettingsData1["orders_email"];
+			$subject = "My Servent - Offer Coupon ";
+			$message = '';
+			$message .= '<body>
+				<div class="container" style=" width:50%;border: 5px solid #fe6003;margin:0 auto">
+				<header style="padding:0.8em;color: white;background-color: #fe6003;clear: left;text-align: center;">
+				 <center><img src='.$base_url . "grocery_admin/uploads/logo/".$getSiteSettingsData1["logo"].' class="logo-responsive"></center>
+				</header>
+				<article style=" border-left: 1px solid gray;overflow: hidden;text-align:justify; word-spacing:0.1px;line-height:25px;padding:15px">
+				  <h1 style="color:#fe6003">Greetings From Myservant</h1>
+				  <p>Dear <span style="color:#fe6003;">'.$user_first_name.'</span>, Thank you for Purchasing Coupons.  myservant.com!</p>
+					<p>Your Offer Coupon Code is: <span style="color:#fe6003;">'.$offer_code.'</span></p>
+					<p>We hope you enjoy your stay at myservant.com, if you have any problems, questions, opinions, praise, comments, suggestions, please free to contact us at any time.</p>
+					<p>Warm Regards,<br>The Myservant Team </p>
+				</article>
+				<footer style="padding: 1em;color: white;background-color: #fe6003;clear: left;text-align: center;">'.$getSiteSettingsData1['footer_text'].'</footer>
+				</div>
+
+				</body>';
+
+			//echo $message; die;
+			//$sendMail = sendEmail($to,$subject,$message,$from);
+			$name = "My Servant - Grocery";
+			$mail = sendEmail($to,$subject,$message,$from,$name);
+
+			//Sending SMS after Purchase coupon
+			$message1 = urlencode('Thank you for purchasing offer coupon. Your Offer Coupon Code is '.$offer_code.''); // Message text required to deliver on mobile number
+		    $sendSMS = sendMobileSMS($message1,$user_phone);
+
         } else {
             // fail query insert problem
             $response["success"] = 1;
